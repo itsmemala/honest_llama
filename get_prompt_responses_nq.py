137bd1c638a667e924603e424d3f300e8453abcb
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-03-05 11:19:50 +0000

    Updates

--- get_prompt_responses_nq.py
+++ get_prompt_responses_nq.py
@@ -14,6 +14,7 @@ import argparse
 # from transformers import BitsAndBytesConfig, GenerationConfig
 # from peft import PeftModel
 # from peft.tuners.lora import LoraLayer
+import evaluate
 
 HF_NAMES = {
     'llama_7B': 'baffo32/decapoda-research-llama-7B-hf',
@@ -43,6 +44,11 @@ def main():
 
     MODEL = HF_NAMES[args.model_name] if not args.model_dir else args.model_dir
 
+    # set seeds
+    torch.manual_seed(42)
+    np.random.seed(42)
+    torch.cuda.manual_seed_all(42)
+
     print('Loading model..')
     tokenizer = llama.LlamaTokenizer.from_pretrained(MODEL)
     model = llama.LlamaForCausalLM.from_pretrained(MODEL, low_cpu_mem_usage=True, torch_dtype=torch.float16, device_map="auto")
@@ -50,12 +56,13 @@ def main():
 
     print('Loading data..')
     # Load data
+    len_dataset = 50 # 3610
     dataset = load_dataset("nq_open", streaming= True)['validation']
     prompts = []
     tokenized_prompts = []
-    for val in list(dataset.take(3610)):
+    for val in list(dataset.take(len_dataset)):
         question = val['question']
-        cur_prompt = f"Q: {question} A: "
+        cur_prompt = f"This is a bot that correctly answers questions. \n Q: {question} A: "
         prompts.append(cur_prompt)
         tokenized_prompt = tokenizer(cur_prompt, return_tensors = 'pt').input_ids
         tokenized_prompts.append(tokenized_prompt)
@@ -63,10 +70,23 @@ def main():
     print('Getting model responses..')
     # Get model responses
     responses = []
+    # period_token_id = tokenizer('. ')['input_ids'][1]
+    period_token_id = tokenizer('.')['input_ids']
+    eos_tokens = ['Question:', ' Question:', '\n', 'Answer:', ' Answer:', 'Q:', ' Q:', 'A:', ' A:',
+                    'QA:', ' QA:', 'QA1', ' QA1', '.\n', ' \n', ':', "\\"]
+    # question_framing_ids = [[tokenizer(eos_token)['input_ids'][1]] for eos_token in eos_tokens]
+    # eos_tokens = ['Question', '\n', 'Answer', ':']
+    question_framing_ids = [tokenizer(eos_token, add_special_tokens=False)['input_ids'] for eos_token in eos_tokens]
+    print('Bad word ids:',question_framing_ids)
     for i,prompt in enumerate(tqdm(tokenized_prompts)):
         prompt = prompt.to(device)
-        response = model.generate(prompt, max_new_tokens=512, num_beams=1, do_sample=False, num_return_sequences=1)[:, prompt.shape[-1]:]
+        response = model.generate(prompt, max_new_tokens=256, num_beams=1, do_sample=False, num_return_sequences=1,
+                                    eos_token_id=period_token_id,
+                                    bad_words_ids=question_framing_ids + [prompt.tolist()[0]]
+                                    )[:, prompt.shape[-1]:]
         response = tokenizer.decode(response[0], skip_special_tokens=True)
+        for check_gen in ['QA2:','Q.', 'B:']
+            response = response.split(check_gen)[0]
         responses.append({'prompt':prompts[i],
                             'response1':response})
     
@@ -78,20 +98,25 @@ def main():
     
     print('Getting labels for model responses..')
     labels = []
-    for i,batch in enumerate(list(dataset.take(3610))): # one row at a time
-        labels_dict['exact_match'] = 0.0
+    rouge = evaluate.load('rouge')
+    exact_match_metric = evaluate.load("exact_match")
+    for i,batch in enumerate(list(dataset.take(len_dataset))): # one row at a time
+        labels_dict = {'exact_match': 0.0,
+                        'rouge1_to_target':0.0,
+                        'rouge2_to_target':0.0,
+                        'rougeL_to_target':0.0}
         reference_answers = batch['answer']
         for answer in reference_answers:
-            predictions = [responses[i].lstrip()]
+            predictions = [responses[i]['response1'].lstrip()]
             references = [answer]
             results = exact_match_metric.compute(predictions=predictions,
                                                     references=references,
                                                     ignore_case=True,
                                                     ignore_punctuation=True)
-            sequence_dict['exact_match'] = max(results['exact_match'], labels_dict['exact_match'])
+            labels_dict['exact_match'] = max(results['exact_match'], labels_dict['exact_match'])
             rouge_results = rouge.compute(predictions=predictions, references=references)
-            for rouge_type in rouge_types:
-                labels_dict[rouge_type + '_to_target'] = max(rouge_results[rouge_type].mid.fmeasure,
+            for rouge_type in ['rouge1','rouge2','rougeL']:
+                labels_dict[rouge_type + '_to_target'] = max(rouge_results[rouge_type],
                                                                 labels_dict[rouge_type + '_to_target'])
 
         labels.append(labels_dict)
