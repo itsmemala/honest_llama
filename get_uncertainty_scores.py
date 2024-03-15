f0080e6a429c2ccd4099aa5937110915b14a6004
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-03-15 18:17:46 +0000

    Create get_uncertainty_scores.py

---
+++ get_uncertainty_scores.py
@@ -0,0 +1,85 @@
+import os
+import torch
+import datasets
+from datasets import load_dataset
+from tqdm import tqdm
+import numpy as np
+import pickle
+import json
+import jsonlines
+import random
+import llama
+import argparse
+# from transformers import BitsAndBytesConfig, GenerationConfig
+# from peft import PeftModel
+# from peft.tuners.lora import LoraLayer
+import evaluate
+
+HF_NAMES = {
+    'llama_7B': 'baffo32/decapoda-research-llama-7B-hf',
+    'honest_llama_7B': 'validation/results_dump/llama_7B_seed_42_top_48_heads_alpha_15',
+    'alpaca_7B': 'circulus/alpaca-7b', 
+    'vicuna_7B': 'AlekseyKorshuk/vicuna-7b', 
+    'llama2_chat_7B': 'meta-llama/Llama-2-7b-chat-hf', 
+    'llama2_chat_13B': 'meta-llama/Llama-2-13b-chat-hf', 
+    'llama2_chat_70B': 'meta-llama/Llama-2-70b-chat-hf', 
+    'flan_33B': 'timdettmers/qlora-flan-33b'
+}
+
+def main(): 
+    """
+    Specify dataset name as the first command line argument. Current options are 
+    "tqa_mc2", "piqa", "rte", "boolq", "copa". Gets activations for all prompts in the 
+    validation set for the specified dataset on the last token for llama-7B. 
+    """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('model_name', type=str, default='llama_7B')
+    parser.add_argument('dataset_name', type=str, default='nq_open', help='dataset for querying the model')
+    parser.add_argument('--device', type=int, default=0)
+    parser.add_argument("--model_dir", type=str, default=None, help='local directory with model data')
+    parser.add_argument("--model_cache_dir", type=str, default=None, help='local directory with model cache')
+    parser.add_argument("--file_name", type=str, default=None, help='local directory with dataset')
+    parser.add_argument('--save_path',type=str, default='')
+    args = parser.parse_args()
+
+    MODEL = HF_NAMES[args.model_name] if not args.model_dir else args.model_dir
+
+    # set seeds
+    torch.manual_seed(42)
+    np.random.seed(42)
+    torch.cuda.manual_seed_all(42)
+
+    print('Loading model..')
+    tokenizer = llama.LlamaTokenizer.from_pretrained(MODEL)
+    model = llama.LlamaForCausalLM.from_pretrained(MODEL, low_cpu_mem_usage=True, torch_dtype=torch.float16, device_map="auto")
+    device = "cuda"
+
+    print('Loading model responses..')
+    data = []
+    with open(f'{args.save_path}/responses/{args.model_name}_{args.dataset_name}_{args.file_name}.json', 'r') as read_file:
+        for line in read_file:
+            data.append(json.loads(line))
+    
+    print('Getting token probability scores..')
+    # Get token probabilities
+    scores = []
+    for i,sample in enumerate(data):
+        prompt = sample['prompt']
+        response = sample['response1']
+        tokenized_input = tokenizer(prompt+response, return_tensors = 'pt').input_ids
+        tokenized_prompt = tokenizer(prompt, return_tensors = 'pt').input_ids
+        # This computation of the negative log likelihoods follows this tutorial: https://huggingface.co/docs/transformers/perplexity
+        target_ids = tokenized_input.clone()
+        target_ids[:len(tokenized_prompt)] = -100
+        model_output = model(torch.reshape(tokenized_input, (1, -1)), labels=target_ids, output_hidden_states=True)
+        average_neg_log_likelihood = model_output['loss'] # len normalised predictive entropy
+        neg_log_likelihood = average_neg_log_likelihood * (len(tokenized_input) - len(tokenized_prompt)) # sequence predictive entropy
+        scores.append([average_neg_log_likelihood,neg_log_likelihood])
+
+    print('Saving token probability scores..')
+    np.save(f'{args.save_path}/uncertainty/{args.model_name}_{args.dataset_name}_uncertainty_scores.npy', scores)
+    
+
+if __name__ == '__main__':
+    main()
