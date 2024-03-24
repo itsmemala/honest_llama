7a260aa779b49ff65d5b1f4c6a30e388bd13653d
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-03-24 20:49:33 +0000

    Update get_activations_and_probe.py

--- get_activations_and_probe.py
+++ get_activations_and_probe.py
@@ -208,9 +208,10 @@ def main():
                             if args.token in ['answer_last','prompt_last','maxpool_all']:
                                 targets = batch['labels']
                             elif args.token=='all':
-                                targets = torch.concatenate([torch.Tensor([y_label for j in range(len(prompt_tokens[idx]))]) for y_label in batch['labels']],axis=0)
+                                targets = torch.concatenate([torch.Tensor([y_label for j in range(len(prompt_tokens[idx]))]) for idx,y_label in zip(batch['inputs_idxs'],batch['labels'])],axis=0)
+                                print(targets.shape)
                             if args.token=='tagged_all':
-                                targets = torch.concatenate([torch.Tensor([y_label for j in range(num_tagged_tokens(tagged_token_idxs[idx]))]) for y_label in batch['labels']],axis=0)
+                                targets = torch.concatenate([torch.Tensor([y_label for j in range(num_tagged_tokens(tagged_token_idxs[idx]))]) for idx,y_label in zip(batch['inputs_idxs'],batch['labels'])],axis=0)
                             outputs = linear_model(inputs)
                             loss = criterion(outputs, targets.to(device))
                             train_loss.append(loss)
