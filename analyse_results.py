3580d2d196872a931665cc597bd4e5f06a0816fc
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-03-25 12:51:11 +0000

    Update analyse_results.py

--- analyse_results.py
+++ analyse_results.py
@@ -6,11 +6,17 @@ import pickle
 import json
 from sklearn.metrics import accuracy_score, f1_score, precision_recall_fscore_support
 from matplotlib import pyplot as plt
+import argparse
 
 def main():
 
-    all_train_loss = np.load(f'{args.save_path}/probes/{args.model_name}_{args.train_file_name}_{args.len_dataset}_{args.num_folds}_{args.using_act}_{args.token}_{args.method}_train_loss.npy')
-    all_test_f1s = np.load(f'{args.save_path}/probes/{args.model_name}_{args.train_file_name}_{args.len_dataset}_{args.num_folds}_{args.using_act}_{args.token}_{args.method}_test_f1.npy')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--results_file_name", type=str, default=None, help='local directory with dataset')
+    parser.add_argument('--save_path',type=str, default='')
+    args = parser.parse_args()
+
+    all_train_loss = np.load(f'{args.save_path}/probes/{args.results_file_name}_train_loss.npy')
+    all_test_f1s = np.load(f'{args.save_path}/probes/{args.results_file_name}_test_f1.npy')
     for fold in range(len(all_test_f1s)):
         print('FOLD',fold,'RESULTS:')
         print('Average:',np.mean(all_test_f1s[fold]))
