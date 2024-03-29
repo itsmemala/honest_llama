a703aa8746a031574f60ed661573e7501f0dee0d
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-03-29 19:07:16 +0000

    script to combine files

--- combine_response_files.py
+++ combine_response_files.py
@@ -17,15 +17,32 @@ def main():
     args = parser.parse_args()
     
     labels_data = []
-    for end in []:
+    for end in [500,1500,3000,6800]:
         with open(f'{args.save_path}/responses/llama_7B_trivia_qa_greedy_responses_labels_train{end}.json', 'r') as read_file:
             for line in read_file:
                 labels_data.append(json.loads(line))
+    with open(f'{args.save_path}/responses/llama_7B_trivia_qa_greedy_responses_labels_train5000.json', 'w') as outfile:
+        for entry in labels_data[:5000]:
+            json.dump(entry, outfile)
+            outfile.write('\n')
+    with open(f'{args.save_path}/responses/llama_7B_trivia_qa_greedy_responses_labels_train1800.json', 'w') as outfile:
+        for entry in labels_data[5000:1800]:
+            json.dump(entry, outfile)
+            outfile.write('\n')
     
-    
-    
-    
-    
+    response_data = []
+    for end in [500,1500,3000,6800]:
+        with open(f'{args.save_path}/responses/llama_7B_trivia_qa_greedy_responses_train{end}.json', 'r') as read_file:
+            for line in read_file:
+                labels_data.append(json.loads(line))
+    with open(f'{args.save_path}/responses/llama_7B_trivia_qa_greedy_responses_train5000.json', 'w') as outfile:
+        for entry in response_data[:5000]:
+            json.dump(entry, outfile)
+            outfile.write('\n')
+    with open(f'{args.save_path}/responses/llama_7B_trivia_qa_greedy_responses_train1800.json', 'w') as outfile:
+        for entry in response_data[5000:1800]:
+            json.dump(entry, outfile)
+            outfile.write('\n')
 
 if __name__ == '__main__':
     main()
