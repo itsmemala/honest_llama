fdfa95a36a9374253d7d00f3ceea067f030926b6
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-03-30 10:42:50 +0000

    Update analyse_results.py

--- analyse_results.py
+++ analyse_results.py
@@ -26,8 +26,8 @@ def main():
         print('Average:',np.mean(all_test_f1s[fold]))
         print('Best:',all_test_f1s[fold][np.argmax(all_val_f1s[fold])])
         confident_sample_pred = []
+        print(all_test_pred[fold].shape)
         for i in range(all_test_pred[fold].shape[1]):
-            print(all_test_pred[fold].shape)
             sample_pred = np.squeeze(all_test_pred[fold][:,i,:]) # Get predictions of each sample across all layers of model
             probe_wise_entropy = (-sample_pred*np.log2(sample_pred)).sum(axis=1)
             confident_sample_pred.append(np.argmax(sample_pred[np.argmin(probe_wise_entropy)]))
