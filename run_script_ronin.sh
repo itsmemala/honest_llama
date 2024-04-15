ba8aab05612c25614b47bc1f2c29c3e012705cc6
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-04-15 17:02:14 +0100

    Create run_script_ronin.sh

---
+++ run_script_ronin.sh
@@ -0,0 +1,13 @@
+python get_activations.py llama_7B trivia_qa --token answer_last --file_name trivia_qa_greedy_responses_train5000 --save_path ~/Desktop/honest_llama_data
+python get_activations.py llama_7B trivia_qa --token prompt_last --file_name trivia_qa_greedy_responses_train5000 --save_path ~/Desktop/honest_llama_data
+python get_activations.py llama_7B trivia_qa --token maxpool_all --file_name trivia_qa_greedy_responses_train5000 --save_path ~/Desktop/honest_llama_data
+python get_activations.py llama_7B trivia_qa --mlp_l1 Yes --token answer_last --file_name trivia_qa_greedy_responses_train5000 --save_path ~/Desktop/honest_llama_data
+python get_activations.py llama_7B trivia_qa --mlp_l1 Yes --token prompt_last --file_name trivia_qa_greedy_responses_train5000 --save_path ~/Desktop/honest_llama_data
+python get_activations.py llama_7B trivia_qa --mlp_l1 Yes --token maxpool_all --file_name trivia_qa_greedy_responses_train5000 --save_path ~/Desktop/honest_llama_data
+
+python get_activations.py llama_7B trivia_qa --token answer_last --file_name trivia_qa_greedy_responses_validation1800 --save_path ~/Desktop/honest_llama_data
+python get_activations.py llama_7B trivia_qa --token prompt_last --file_name trivia_qa_greedy_responses_validation1800 --save_path ~/Desktop/honest_llama_data
+python get_activations.py llama_7B trivia_qa --token maxpool_all --file_name trivia_qa_greedy_responses_validation1800 --save_path ~/Desktop/honest_llama_data
+python get_activations.py llama_7B trivia_qa --mlp_l1 Yes --token answer_last --file_name trivia_qa_greedy_responses_validation1800 --save_path ~/Desktop/honest_llama_data
+python get_activations.py llama_7B trivia_qa --mlp_l1 Yes --token prompt_last --file_name trivia_qa_greedy_responses_validation1800 --save_path ~/Desktop/honest_llama_data
+python get_activations.py llama_7B trivia_qa --mlp_l1 Yes --token maxpool_all --file_name trivia_qa_greedy_responses_validation1800 --save_path ~/Desktop/honest_llama_data
