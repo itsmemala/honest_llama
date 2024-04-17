991075c50ff4bc8ccde8d0079bb6dcddf6fbdef4
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-04-17 14:13:16 +0100

    Add analysis using logits

--- analyse_results.py
+++ analyse_results.py
@@ -38,12 +38,25 @@ def main():
     all_val_f1s = np.load(f'{args.save_path}/probes/{args.results_file_name}_val_f1.npy')
     all_test_pred, all_test_true = np.load(f'{args.save_path}/probes/{args.results_file_name}_test_pred.npy'), np.load(f'{args.save_path}/probes/{args.results_file_name}_test_true.npy')
     all_val_pred, all_val_true = np.load(f'{args.save_path}/probes/{args.results_file_name}_val_pred.npy'), np.load(f'{args.save_path}/probes/{args.results_file_name}_val_true.npy')
+    all_val_logits, all_test_logits = np.load(f'{args.save_path}/probes/{args.results_file_name}_val_logits.npy'), np.load(f'{args.save_path}/probes/{args.results_file_name}_test_logits.npy')
+
     for fold in range(len(all_test_f1s)):
-        assert sum(all_test_true[fold][0]==all_test_true[fold][1])==len(all_test_true[fold][0]) # check all models have same batch order
+
+        for model in all_test_true[fold].shape[0]:
+            assert sum(all_test_true[fold][0]==all_test_true[fold][model])==len(all_test_true[fold][0]) # check all models have same batch order
+            assert sum(all_val_true[fold][0]==all_val_true[fold][model])==len(all_val_true[fold][0])
+
+        test_f1_using_logits, val_f1_using_logits = [], []
+        for model in all_test_logits[fold].shape[0]:
+            test_f1_using_logits.append(f1_score(all_test_true[fold][0],np.argmax(all_test_logits[fold][model], axis=1)))
+            val_f1_using_logits.append(f1_score(all_val_true[fold][0],np.argmax(all_val_logits[fold][model], axis=1)))
+
         print('FOLD',fold,'RESULTS:')
         print('Average:',np.mean(all_test_f1s[fold]))
         print('Best:',all_test_f1s[fold][np.argmax(all_val_f1s[fold])],np.argmax(all_val_f1s[fold]))
+        print('Average using logits:',np.mean(test_f1_using_logits))
         
+        # Oracle 1
         print('\n')
         best_sample_pred =[]
         num_correct_probes_nonhallu, correct_probes_nonhallu = [], []
@@ -91,6 +104,7 @@ def main():
         print('# Hallucinated samples:',sum(all_test_true[fold][0]==0))
         print('\n')
         
+        # Oracle 2
         print('\n')
         best_sample_pred =[]
         best_probes_nonhallu, correct_probes_nonhallu = [], []
@@ -132,6 +146,7 @@ def main():
         print('Oracle (using 5 most confident):',f1_score(all_test_true[fold][0],best_sample_pred),f1_score(all_test_true[fold][0],best_sample_pred,pos_label=0))
         print('\n')
 
+        # Oracle 3
         print('\n')
         best_sample_pred =[]
         best_probe_idxs = np.argpartition(all_val_f1s[fold], -5)[-5:]
@@ -149,6 +164,72 @@ def main():
         print('Oracle (using 5 most accurate):',f1_score(all_test_true[fold][0],best_sample_pred),f1_score(all_test_true[fold][0],best_sample_pred,pos_label=0))
         print('\n')
         
+        # Oracle 1 - using logits
+        print('\n')
+        best_sample_pred =[]
+        num_correct_probes_nonhallu, correct_probes_nonhallu = [], []
+        num_correct_probes_hallu, correct_probes_hallu = [], []
+        for i in range(all_test_logits[fold].shape[1]):
+            sample_pred = np.squeeze(all_test_logits[fold][:,i,:]) # Get predictions of each sample across all layers of model
+            sample_pred = np.argmax(sample_pred,axis=1)
+            assert sample_pred.shape==(32,) # num_layers
+            correct_answer = all_test_true[fold][0][i]
+            if correct_answer==1: num_correct_probes_nonhallu.append(sum(sample_pred==correct_answer))
+            if correct_answer==1 and sum(sample_pred==correct_answer)<20: correct_probes_nonhallu += [idx for idx,probe_pred in enumerate(sample_pred) if probe_pred==correct_answer]
+            if correct_answer==0: num_correct_probes_hallu.append(sum(sample_pred==correct_answer))
+            if correct_answer==0 and sum(sample_pred==correct_answer)<20: correct_probes_hallu += [idx for idx,probe_pred in enumerate(sample_pred) if probe_pred==correct_answer]
+            # if i==0: print(sample_pred==correct_answer,sum(sample_pred==correct_answer))
+            if sum(sample_pred==correct_answer)>0:
+                best_sample_pred.append(correct_answer)
+            else:
+                best_sample_pred.append(1 if correct_answer==0 else 0)
+        assert f1_score(all_test_true[fold][0],all_test_true[fold][0])==1
+        fig, axs = plt.subplots(2,2)
+        counts, bins = np.histogram(num_correct_probes_nonhallu, bins=range(33))
+        axs[0,0].stairs(counts, bins)
+        axs[0,0].title.set_text('Non-Hallucinated')
+        axs[0,0].set_xlabel('# probes classifying correctly')
+        axs[0,0].set_ylabel('# samples')
+        counts, bins = np.histogram(num_correct_probes_hallu, bins=range(33))
+        axs[0,1].stairs(counts, bins)
+        axs[0,1].title.set_text('Hallucinated')
+        axs[0,1].set_xlabel('# probes classifying correctly')
+        counts, bins = np.histogram(correct_probes_nonhallu, bins=range(33))
+        axs[1,0].stairs(counts, bins)
+        axs[1,0].set_xlabel('probe idx')
+        axs[1,0].set_ylabel('# samples correct\n(when num_correct_probes<20)')
+        counts, bins = np.histogram(correct_probes_hallu, bins=range(33))
+        axs[1,1].stairs(counts, bins)
+        axs[1,1].set_xlabel('probe idx')
+        fig.tight_layout()
+        fig.savefig(f'{args.save_path}/figures/{args.results_file_name}_oracle_hist_using_logits.png')
+        print('Oracle using logits:',f1_score(all_test_true[fold][0],best_sample_pred),f1_score(all_test_true[fold][0],best_sample_pred,pos_label=0))
+        num_correct_probes_nonhallu = np.array(num_correct_probes_nonhallu)
+        num_correct_probes_hallu = np.array(num_correct_probes_hallu)
+        print('Non-Hallucinated hard samples:',sum(num_correct_probes_nonhallu<20),sum(num_correct_probes_nonhallu<10),sum(num_correct_probes_nonhallu<5))
+        print('Hallucinated hard samples:',sum(num_correct_probes_hallu<20),sum(num_correct_probes_hallu<10),sum(num_correct_probes_hallu<5))
+        print('# Hallucinated samples:',sum(all_test_true[fold][0]==0))
+        print('\n')
+
+        # Oracle 3 - using logits
+        print('\n')
+        best_sample_pred =[]
+        best_probe_idxs = np.argpartition(val_f1_using_logits, -5)[-5:]
+        top_5_lower_bound_val = np.min(val_f1_using_logits[best_probe_idxs])
+        print('Best probes:',best_probe_idxs)
+        for i in range(all_test_logits[fold].shape[1]):
+            sample_pred = np.squeeze(all_test_logits[fold][:,i,:]) # Get predictions of each sample across all layers of model
+            sample_pred = sample_pred[val_f1_using_logits>=top_5_lower_bound_val]
+            sample_pred = np.argmax(sample_pred,axis=1)
+            correct_answer = all_test_true[fold][0][i]
+            if sum(sample_pred==correct_answer)>0:
+                best_sample_pred.append(correct_answer)
+            else:
+                best_sample_pred.append(1 if correct_answer==0 else 0)
+        print('Oracle (using 5 most accurate) using logits:',f1_score(all_test_true[fold][0],best_sample_pred),f1_score(all_test_true[fold][0],best_sample_pred,pos_label=0))
+        print('\n')
+
+        # Probe selection - a
         confident_sample_pred = []
         # print(all_test_pred[fold].shape)
         for i in range(all_test_pred[fold].shape[1]):
@@ -174,6 +255,8 @@ def main():
         #     probe_wise_entropy = (-sample_pred*np.nan_to_num(np.log2(sample_pred),neginf=0)).sum(axis=1)[best_probes]
         #     confident_sample_pred.append(np.argmax(sample_pred[np.argmin(probe_wise_entropy)]))
         # print('Using most confident probe per sample (best probes by loss):',f1_score(all_test_true[fold][0],confident_sample_pred))
+
+        # Probe selection - b
         confident_sample_pred1, confident_sample_pred2 = [], []
         # print(all_test_pred[fold].shape)
         for i in range(all_test_pred[fold].shape[1]):
@@ -190,6 +273,7 @@ def main():
             confident_sample_pred2.append(any_vote)
         print('Voting amongst 5 most confident probes per sample:',f1_score(all_test_true[fold][0],confident_sample_pred1),f1_score(all_test_true[fold][0],confident_sample_pred1,pos_label=0))
         print('Any one amongst 5 most confident probes per sample:',f1_score(all_test_true[fold][0],confident_sample_pred2),f1_score(all_test_true[fold][0],confident_sample_pred2,pos_label=0))
+        # Probe selection - c
         confident_sample_pred = []
         for i in range(all_test_pred[fold].shape[1]):
             sample_pred = np.squeeze(all_test_pred[fold][:,i,:]) # Get predictions of each sample across all layers of model
@@ -198,6 +282,15 @@ def main():
             confident_sample_pred.append(maj_vote)
         print('Voting amongst all probes per sample:',f1_score(all_test_true[fold][0],confident_sample_pred),precision_recall_fscore_support(all_test_true[fold][0],confident_sample_pred))
         
+        # Probe selection - c - using logits
+        confident_sample_pred = []
+        for i in range(all_test_logits[fold].shape[1]):
+            sample_pred = np.squeeze(all_test_logits[fold][:,i,:]) # Get predictions of each sample across all layers of model
+            class_1_vote_cnt = sum(np.argmax(sample_pred,axis=1))
+            maj_vote = 1 if class_1_vote_cnt>=(sample_pred.shape[0]/2) else 0
+            confident_sample_pred.append(maj_vote)
+        print('Voting amongst all probes per sample - using logits:',f1_score(all_test_true[fold][0],confident_sample_pred),precision_recall_fscore_support(all_test_true[fold][0],confident_sample_pred))
+
         print('\n')
         np.set_printoptions(precision=2)
         for model in range(len(all_val_loss[fold])):
