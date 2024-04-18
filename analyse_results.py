4021f5d52253b8b6a1426b5bf6c4ec3cecc44ab5
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-04-18 13:01:04 +0100

    Update analyse_results.py

--- analyse_results.py
+++ analyse_results.py
@@ -291,7 +291,7 @@ def main():
             confident_sample_pred.append(maj_vote)
         print('Voting amongst most accurate 5 probes:',f1_score(all_test_true[fold][0],confident_sample_pred),f1_score(all_test_true[fold][0],confident_sample_pred,pos_label=0))
         # Probe selection - f
-        for sim_cutoff in [0.3,0.4,0.5,0.6,0.7,0.8,0.9]:
+        for sim_cutoff in [0.3,0.4,0.5,0.6]:
             confident_sample_pred1, confident_sample_pred2, selected_probes = [], [], []
             for i in range(all_test_pred[fold].shape[1]):
                 sample_pred = np.squeeze(all_test_pred[fold][:,i,:]) # Get predictions of each sample across all layers of model
@@ -300,7 +300,7 @@ def main():
                 selected_probes.append(sum(best_probe_idxs))
                 # method 1 - vote
                 class_1_vote_cnt = sum(np.argmax(sample_pred_chosen,axis=1))
-                maj_vote = 1 if class_1_vote_cnt>2 else 0
+                maj_vote = 1 if class_1_vote_cnt>(sum(best_probe_idxs)/2) else 0
                 confident_sample_pred1.append(maj_vote)
                 # method 2 - choose most confident
                 probe_wise_entropy = (-sample_pred_chosen*np.nan_to_num(np.log2(sample_pred_chosen),neginf=0)).sum(axis=1)
