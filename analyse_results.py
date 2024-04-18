81da400a841c53a52d30ab97ce5012055d9cb0b8
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-04-18 14:00:23 +0100

    Update analyse_results.py

--- analyse_results.py
+++ analyse_results.py
@@ -310,6 +310,10 @@ def main():
                 confident_sample_pred2.append(np.argmax(sample_pred_chosen[np.argmin(probe_wise_entropy)]))
             print('Voting amongst most similar probes per sample (>',sim_cutoff,'):',f1_score(all_test_true[fold][0],confident_sample_pred1),f1_score(all_test_true[fold][0],confident_sample_pred1,pos_label=0))
             print('Using most confident amongst most similar probes per sample (>',sim_cutoff,'):',f1_score(all_test_true[fold][0],confident_sample_pred2),f1_score(all_test_true[fold][0],confident_sample_pred2,pos_label=0))
+        counts, bins = np.histogram(all_test_sim[fold][0,:,0])
+        plt.stairs(counts, bins)
+        plt.savefig(f'{args.save_path}/figures/{args.results_file_name}_sim_scores.png')
+        
         print('\n')
         # Probe selection - d - using logits
         confident_sample_pred = []
