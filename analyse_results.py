d76b1fedcbea00e9140d699d73d419de78110cce
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-04-26 14:57:07 +0100

    Update analyse_results.py

--- analyse_results.py
+++ analyse_results.py
@@ -553,45 +553,78 @@ def main():
 
         # Probe selection - l
         confident_sample_pred, confident_sample_pred2 = [], []
-        best_probe_idxs = np.argpartition(all_val_f1s[fold], -10)[-10:]
+        best_probe_idxs = np.argpartition(all_val_f1s[fold], -5)[-5:]
         top_5_lower_bound_val = np.min(all_val_f1s[fold][best_probe_idxs])
-        best_probe_idxs2 = np.argpartition(val_f1_avg, -10)[-10:]
+        best_probe_idxs2 = np.argpartition(val_f1_avg, -5)[-5:]
         top_5_lower_bound_val2 = np.min(val_f1_avg[best_probe_idxs2])
         # print(sum(val_f1_avg>=top_5_lower_bound_val2))
-        num_hard_samples, entropy_gap, entropy_gap_to_correct, entropy_gap2, entropy_gap_to_correct2 = 0, [], [], [], []
+        num_hard_samples, entropy_wrong, entropy_gap, entropy_gap_to_correct, entropy_gap2, entropy_gap_to_correct2 = 0, [], [], [], [], []
+        max_sim, max_sim1 = [], []
         for i in range(all_test_pred[fold].shape[1]):
             sample_pred = np.squeeze(all_test_pred[fold][:,i,:]) # Get predictions of each sample across all layers of model
             sample_pred_chosen = sample_pred[all_val_f1s[fold]>=top_5_lower_bound_val]
             probe_wise_entropy = (-sample_pred_chosen*np.nan_to_num(np.log2(sample_pred_chosen),neginf=0)).sum(axis=1)
             confident_sample_pred.append(np.argmax(sample_pred_chosen[np.argmin(probe_wise_entropy)]))
+            
             sample_pred2_chosen = np.squeeze(all_test_pred[fold][:,i,:])[val_f1_avg>=top_5_lower_bound_val2]
             probe_wise_entropy = (-sample_pred2_chosen*np.nan_to_num(np.log2(sample_pred2_chosen),neginf=0)).sum(axis=1)
             confident_sample_pred2.append(np.argmax(sample_pred2_chosen[np.argmin(probe_wise_entropy)]))
+            all_correct_index = np.argwhere(np.argmax(sample_pred2_chosen,axis=1)==all_test_true[fold][0][i]) # 0-4
             if np.argmax(sample_pred2_chosen[np.argmin(probe_wise_entropy)])!=all_test_true[fold][0][i]:
                 # print(probe_wise_entropy)
                 num_hard_samples += 1
+                entropy_wrong.append(np.min(probe_wise_entropy))
                 entropy_gap.append((probe_wise_entropy[np.argpartition(probe_wise_entropy,2)[1]]-np.min(probe_wise_entropy))/np.min(probe_wise_entropy))
-                if sum(np.argmax(sample_pred2_chosen,axis=1)==all_test_true[fold][0][i])>0: # if correct prediction exists
-                    entropy_gap_to_correct.append((probe_wise_entropy[np.argwhere(np.argmax(sample_pred2_chosen,axis=1)==all_test_true[fold][0][i])[0]]-np.min(probe_wise_entropy))/np.min(probe_wise_entropy))
-            if np.argmax(sample_pred2_chosen[np.argmin(probe_wise_entropy)])==all_test_true[fold][0][i]:
+                if len(all_correct_index)>0: # if correct prediction exists
+                    entropy_gap_to_correct.append((np.min(probe_wise_entropy[all_correct_index])-np.min(probe_wise_entropy))/np.min(probe_wise_entropy))
+            else:
                 num_hard_samples += 1
                 entropy_gap2.append((probe_wise_entropy[np.argpartition(probe_wise_entropy,2)[1]]-np.min(probe_wise_entropy))/np.min(probe_wise_entropy))
-                if sum(np.argmax(sample_pred2_chosen,axis=1)==all_test_true[fold][0][i])>1: # if more than 1 correct prediction exists
-                    entropy_gap_to_correct2.append((probe_wise_entropy[np.argwhere(np.argmax(sample_pred2_chosen,axis=1)==all_test_true[fold][0][i])[0]]-np.min(probe_wise_entropy))/np.min(probe_wise_entropy))
-            mc_index = np.argwhere(val_f1_avg>=top_5_lower_bound_val2)[np.argmin(probe_wise_entropy)]
-            print(mc_index)
-            break
+                if len(all_correct_index)>1: # if more than 1 correct prediction exists
+                    correct_index_excl_mc = [idx for idx in all_correct_index if idx!=np.argmin(probe_wise_entropy)]
+                    entropy_gap_to_correct2.append((np.min(probe_wise_entropy[correct_index_excl_mc])-np.min(probe_wise_entropy))/np.min(probe_wise_entropy))
+            ma5_index = np.argwhere(val_f1_avg>=top_5_lower_bound_val2) # 0-31
+            mc_index = ma5_index[np.argmin(probe_wise_entropy)] # 0-31
             mc_wgts_cls0, mc_wgts_cls1 = get_probe_wgts(fold,mc_index,args.results_file_name,args.save_path)
-        print(len(sample_pred2_chosen))
+            max_sim_val, max_sim_val1 = -1, -1
+            if len(all_correct_index)>0: # if correct prediction exists
+                for idx_a in all_correct_index: # for each correct probe
+                    correct_index = ma5_index[idx_a]
+                    wgts_cls0_a, wgts_cls1_a = get_probe_wgts(fold,correct_index,args.results_file_name,args.save_path)
+                    norm_weights_a = wgts_cls1_a/wgts_cls1_a.pow(2).sum(dim=-1).sqrt().unsqueeze(-1) # unit normalise
+                    for idx_b in ma5_index:
+                        if idx_b not in ma5_index[all_correct_index]: # for each incorrect probe
+                            wgts_cls0_b, wgts_cls1_b = get_probe_wgts(fold,idx_b,args.results_file_name,args.save_path)
+                            norm_weights_b = wgts_cls1_b/wgts_cls1_b.pow(2).sum(dim=-1).sqrt().unsqueeze(-1) # unit normalise
+                            sim = norm_weights_a*norm_weights_b # sim of correct and incorrect probes
+                            if sim>max_sim_val: max_sim_val = sim
+                max_sim.append(max_sim_val)
+            else:
+                for idx_a in ma5_index:
+                    wgts_cls0_a, wgts_cls1_a = get_probe_wgts(fold,idx_a,args.results_file_name,args.save_path)
+                    norm_weights_a = wgts_cls1_a/wgts_cls1_a.pow(2).sum(dim=-1).sqrt().unsqueeze(-1) # unit normalise
+                    for idx_b in ma5_index:
+                        if idx_b != idx_a: # for each incorrect probe
+                            wgts_cls0_b, wgts_cls1_b = get_probe_wgts(fold,idx_b,args.results_file_name,args.save_path)
+                            norm_weights_b = wgts_cls1_b/wgts_cls1_b.pow(2).sum(dim=-1).sqrt().unsqueeze(-1) # unit normalise
+                            sim = norm_weights_a*norm_weights_b # sim of probes
+                            if sim>max_sim_val1: max_sim_val1 = sim
+                max_sim1.append(max_sim_val1)
+        # print(len(sample_pred2_chosen))
         print('MC amongst most accurate (for cls1) 5 probes:',f1_score(all_test_true[fold][0],confident_sample_pred),f1_score(all_test_true[fold][0],confident_sample_pred,pos_label=0))
         print('MC amongst most accurate (for both cls) 5 probes:',f1_score(all_test_true[fold][0],confident_sample_pred2),f1_score(all_test_true[fold][0],confident_sample_pred2,pos_label=0))
         # print(num_hard_samples)
-        fig, axs = plt.subplots(1,1)
-        counts, bins = np.histogram(entropy_gap)
+        # fig, axs = plt.subplots(1,1)
+        # counts, bins = np.histogram(entropy_gap)
+        print(np.histogram(entropy_wrong))
+        print('Entropy gaps:')
         print(np.histogram(entropy_gap))
         print(np.histogram(entropy_gap_to_correct))
         print(np.histogram(entropy_gap2))
         print(np.histogram(entropy_gap_to_correct2))
+        print('Probe similarity:')
+        print(np.histogram(sim_val))
+        print(np.histogram(sim_val1))
         # axs.stairs(counts, bins)
         # fig.savefig(f'{args.save_path}/figures/{args.results_file_name}_entropy_gap.png')
 
