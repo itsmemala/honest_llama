92c5ffe86ffdf8c5958d7f3bdb98ce4988cf575b
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-04-29 08:14:02 +0100

    Update analyse_results.py

--- analyse_results.py
+++ analyse_results.py
@@ -711,9 +711,10 @@ def main():
         probe_wgts_cls0 = [val.detach().cpu().numpy() for val in probe_wgts_cls0]
         # print('Probe dimensions:')
         # print(np.histogram(np.argmax(probe_wgts_cls0, axis=1)))
+        print('PCA:')
         pca = KernelPCA(n_components=100, kernel='poly') # PCA(n_components=3)
         transformed = pca.fit_transform(probe_wgts_cls0)
-        print(transformed.shape)
+        print(transformed[0].shape)
         
         # print('\n')
         # # Probe selection - a - using logits
