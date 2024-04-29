6b47caf2a8509a2a687d116fe2e2ca7731bae78c
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-04-29 19:46:13 +0100

    Update get_activations_and_probe.py

--- get_activations_and_probe.py
+++ get_activations_and_probe.py
@@ -482,6 +482,7 @@ def main():
                                 y_val_true += batch['labels'].tolist()
                                 val_preds_batch = F.softmax(linear_model(inputs).data, dim=1) if args.token in ['answer_last','prompt_last','maxpool_all'] else torch.stack([torch.max(F.softmax(linear_model(inp).data, dim=1), dim=0)[0] for inp in inputs]) # For each sample, get max prob per class across tokens
                                 val_preds.append(val_preds_batch)
+                                if args.token in ['all','tagged_tokens']: inputs = torch.cat(activations,dim=0)  # stack for calculating sim
                                 val_sim.append(torch.stack((torch.sum(inputs * norm_weights_0.detach(), dim=-1),torch.sum(inputs * norm_weights_1.detach(), dim=-1)),dim=1)) # + linear_model.linear.bias
                         all_val_preds[i].append(torch.cat(val_preds).cpu().numpy())
                         all_val_sim[i].append(torch.cat(val_sim).cpu().numpy())
@@ -516,6 +517,7 @@ def main():
                                 test_preds_batch = F.softmax(linear_model(inputs).data, dim=1) if args.token in ['answer_last','prompt_last','maxpool_all'] else torch.stack([torch.max(F.softmax(linear_model(inp).data, dim=1), dim=0)[0] for inp in inputs]) # For each sample, get max prob per class across tokens
                                 test_preds.append(test_preds_batch)
                                 test_logits.append(linear_model(inputs))
+                                if args.token in ['all','tagged_tokens']: inputs = torch.cat(activations,dim=0)  # stack for calculating sim
                                 test_sim.append(torch.stack((torch.sum(inputs * norm_weights_0.detach(), dim=-1),torch.sum(inputs * norm_weights_1.detach(), dim=-1)),dim=1)) # + linear_model.linear.bias
                         all_test_preds[i].append(torch.cat(test_preds).cpu().numpy())
                         all_test_sim[i].append(torch.cat(test_sim).cpu().numpy())
