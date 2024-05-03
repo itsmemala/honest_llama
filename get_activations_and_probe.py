cd6297d8c8143c0a0541453c10309736bf205a46
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-05-03 16:31:52 +0100

    Update get_activations_and_probe.py

--- get_activations_and_probe.py
+++ get_activations_and_probe.py
@@ -502,7 +502,7 @@ def main():
                                     file_path = f'{args.save_path}/features/{args.model_name}_{args.dataset_name}_{args.token}/{args.model_name}_{args.train_file_name}_{args.token}_{act_type[args.using_act]}_{file_end}.pkl'
                                     act = torch.from_numpy(np.load(file_path,allow_pickle=True)[idx%100][layer]).to(device) if 'mlp' in args.using_act or 'layer' in args.using_act else torch.from_numpy(np.load(file_path,allow_pickle=True)[idx%100][layer][head*128:(head*128)+128]).to(device)
                                     acts.append(act)
-                            norm_acts = acts / act.pow(2).sum(dim=1).sqrt().unsqueeze(-1) # unit normalise
+                            norm_acts = acts / acts.pow(2).sum(dim=1).sqrt().unsqueeze(-1) # unit normalise
                             probs = F.softmax(linear_model(norm_acts).data, dim=1)
                             entropy = (-probs*np.nan_to_num(np.log2(probs),neginf=0)).sum(axis=1)
                             model_wise_mc_sample_idxs.append(np.array(hallu_idxs)[entropy<0.2])
