e500fd3c14d8643619f0cc793ac8360a5a3183d9
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-05-06 13:17:59 +0100

    Update get_activations_and_probe.py

--- get_activations_and_probe.py
+++ get_activations_and_probe.py
@@ -467,6 +467,7 @@ def main():
                                     top_sim_acts = norm_acts[torch.topk(sim,args.spl_knn)[1]]
                                     print(top_sim_acts.shape)
                                 print(torch.mean(torch.sum(top_sim_acts * cur_norm_weights_0, dim=-1)))
+                                if epoch==args.epochs: print('Similarity of knn samples at current layer:',torch.sum(top_sim_acts * cur_norm_weights_0, dim=-1))
 
                             # Get val loss
                             linear_model.eval()
