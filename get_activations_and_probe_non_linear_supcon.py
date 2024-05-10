bbd35e48828568b6e4aec9fd3dcd6d375d76688e
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-05-10 17:17:35 +0100

    Update get_activations_and_probe_non_linear_supcon.py

--- get_activations_and_probe_non_linear_supcon.py
+++ get_activations_and_probe_non_linear_supcon.py
@@ -225,15 +225,18 @@ def main():
                     train_loss = []
                     no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
                     named_params = [] # list(nlinear_model.named_parameters())
-                    for n,param in nlinear_model.named_modules()[0].named_parameters(): # we only want index 0 from named_modules() which is a superset module of all params
-                        print(n)
-                        if final_layer_name in n: # Do not train final layer
-                            print('Excluding param from training:',n)
-                            param.requires_grad = False
-                        else:
-                            print('Including param for training:',n)
-                            named_params.append((n,param))
-                            param.requires_grad = True
+                    print(nlinear_model.named_parameters())
+                    # for _,m in nlinear_model.named_modules():
+                    #     for n,param in m.named_parameters(): # we only want index 0 from named_modules() which is a superset module of all params
+                    #         print(n)
+                    #         if final_layer_name in n: # Do not train final layer
+                    #             print('Excluding param from training:',n)
+                    #             param.requires_grad = False
+                    #         else:
+                    #             print('Including param for training:',n)
+                    #             named_params.append((n,param))
+                    #             param.requires_grad = True
+                    #     break
                     optimizer_grouped_parameters = [
                         {'params': [p for n, p in named_params if not any(nd in n for nd in no_decay)], 'weight_decay': 0.00001, 'lr': args.lr},
                         {'params': [p for n, p in named_params if any(nd in n for nd in no_decay)], 'weight_decay': 0.0, 'lr': args.lr}
