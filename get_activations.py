60fc29e8c9c6ce0815965307db82cc8309ca796f
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-05-22 14:31:40 +0100

    Update get_activations.py

--- get_activations.py
+++ get_activations.py
@@ -151,7 +151,8 @@ def main():
             if '5000' in args.file_name:
                 load_ranges = [(a*100,(a*100)+100) for a in range(int(5000/100))] # train file
             else:
-                load_ranges = [(a*100,(a*100)+100) for a in range(int(1800/100)) if a*100>200] # test file
+                # load_ranges = [(a*100,(a*100)+100) for a in range(int(1800/100))] # test file
+                load_ranges = [(a*50,(a*50)+50) for a in range(int(1800/50))] # test file (only for hallucheck3)
     elif args.dataset_name=='cnn_dailymail':
         if args.token=='prompt_last_onwards':
             load_ranges = [(a*20,(a*20)+20) for a in range(int(1000/20)+1) if (a*20)+20>520]
