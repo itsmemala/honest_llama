f5dc43dc204d4358701ef924af757266687fdfe3
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-05-27 15:35:55 +0100

    Update analyse_crossdataset_bce.py

--- analyse_crossdataset_bce.py
+++ analyse_crossdataset_bce.py
@@ -247,11 +247,9 @@ def main():
             sample_pred_cls = np.array([1 if pred>layer_pred_thresholds[layer] else 0 for layer,pred in enumerate(sample_pred)])
             sample_pred = np.concatenate((1-sample_pred[:, None], sample_pred[:, None]),axis=1)
             probe_wise_entropy = (-sample_pred*np.nan_to_num(np.log2(sample_pred),neginf=0)).sum(axis=1)
-            # Note: With BCE, most confident would be the ones with highest probability
-            # best_probe_idxs = np.argpartition(sample_pred, -5)[-5:] # Note: sort is asc, so take last x values for largest x
             best_probe_idxs = np.argpartition(probe_wise_entropy, 5)[:5] # Note: sort is asc, so take first x values for smallest x
-            top_5_lower_bound_val = np.min(sample_pred[best_probe_idxs])
-            best_probe_idxs = sample_pred>=top_5_lower_bound_val
+            top_5_lower_bound_val = np.max(probe_wise_entropy)
+            best_probe_idxs = probe_wise_entropy<=top_5_lower_bound_val
             sample_pred_chosen = sample_pred_cls[best_probe_idxs]
             cls1_vote = np.sum(sample_pred_chosen)/len(sample_pred_chosen)
             vote_distri = np.array([cls1_vote, 1 - cls1_vote])
