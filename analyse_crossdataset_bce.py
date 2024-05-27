ff4865a5cb1d8f1f5584d35a2593b6f1e40ab6a0
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-05-27 16:30:59 +0100

    Update analyse_crossdataset_bce.py

--- analyse_crossdataset_bce.py
+++ analyse_crossdataset_bce.py
@@ -10,6 +10,7 @@ from itertools import combinations
 from sklearn.metrics import accuracy_score, f1_score, precision_recall_fscore_support, precision_score, recall_score, classification_report
 from sklearn.decomposition import PCA, KernelPCA
 from matplotlib import pyplot as plt
+import seaborn as sns
 import argparse
 from utils import LogisticRegression_Torch, tokenized_from_file
 
@@ -99,15 +100,15 @@ def main():
         try:
             all_preds = np.load(f'{args.save_path}/probes/{args.probes_file_name}_{args.responses_file_name}.npy')
         except FileNotFoundError:
-            all_preds, all_preds_by_token = [], []
+            all_preds, = []
             # Get predictions from probes trained on greedy responses
+            args.using_act = 'layer' if 'layer' in args.probes_file_name else 'mlp'
             for layer in range(num_layers):
                 # Load model
                 act_dims = {'mlp':4096,'mlp_l1':11008,'ah':128,'layer':4096}
                 bias = False if 'no_bias' in args.probes_file_name else True
                 head = 0
                 kld_probe = 0
-                linear_model = LogisticRegression_Torch(act_dims[args.using_act], 2, bias=bias).to(device)
                 try:
                     linear_model = torch.load(f'{args.save_path}/probes/models/{args.probes_file_name}_model0_{layer}_{head}_{kld_probe}')
                 except FileNotFoundError:
@@ -122,11 +123,42 @@ def main():
                     act = torch.from_numpy(np.load(file_path,allow_pickle=True)[i%acts_per_file][layer]).to(device) if 'mlp' in args.using_act or 'layer' in args.using_act else torch.from_numpy(np.load(file_path,allow_pickle=True)[idx%acts_per_file][layer][head*128:(head*128)+128]).to(device)
                     acts.append(act)
                 inputs = torch.stack(acts,axis=0) if args.token in ['answer_last','prompt_last','maxpool_all'] else torch.cat(activations,dim=0)
-                if 'unitnorm' in args.probes_file_name: inputs = inputs / inputs.pow(2).sum(dim=-1).sqrt().unsqueeze(-1) # unit normalise
+                if 'unitnorm' in args.probes_file_name or 'individual_linear_orthogonal' in args.probes_file_name or 'individual_linear_specialised' in args.probes_file_name: inputs = inputs / inputs.pow(2).sum(dim=-1).sqrt().unsqueeze(-1) # unit normalise
                 preds = torch.sigmoid(linear_model(inputs).data) if args.token in ['answer_last','prompt_last','maxpool_all'] else torch.stack([torch.max(torch.sigmoid(nlinear_model(inp).data), dim=0)[0] for inp in inputs]) # For each sample, get max prob per class across tokens
                 all_preds.append(preds.cpu().numpy())
             all_preds = np.stack(all_preds)
             np.save(f'{args.save_path}/probes/{args.probes_file_name}_{args.responses_file_name}.npy',all_preds)
+    # Get preds on all tokens
+    try:
+        alltokens_preds = np.load(f'{args.save_path}/probes/{args.probes_file_name}_{args.responses_file_name}_alltokens_preds.npy')
+    except FileNotFoundError:
+        alltokens_preds = []
+        # Get predictions from probes trained on greedy/baseline responses
+        args.using_act = 'layer' if 'layer' in args.probes_file_name else 'mlp'
+        args.token = 'all'
+        for layer in range(num_layers):
+            # Load model
+            act_dims = {'mlp':4096,'mlp_l1':11008,'ah':128,'layer':4096}
+            bias = False if 'no_bias' in args.probes_file_name else True
+            head = 0
+            kld_probe = 0
+            try:
+                linear_model = torch.load(f'{args.save_path}/probes/models/{args.probes_file_name}_model0_{layer}_{head}_{kld_probe}')
+            except FileNotFoundError:
+                linear_model = torch.load(f'{args.save_path}/probes/models/{args.probes_file_name}_model0_{layer}_{head}')
+            linear_model.eval()
+            # Load activations
+            acts = []
+            for i in samples_neg_affected[:10] + samples_pos_affected[:10]:
+                act_type = {'mlp':'mlp_wise','mlp_l1':'mlp_l1','ah':'head_wise','layer':'layer_wise'}
+                file_end = i-(i%acts_per_file)+acts_per_file # 487: 487-(87)+100
+                file_path = f'{args.save_path}/features/{args.model_name}_{args.dataset_name}_{args.token}/{args.model_name}_{args.dataset_name}_{args.responses_file_name}_{args.token}_{act_type[args.using_act]}_{file_end}.pkl'
+                acts = torch.from_numpy(np.load(file_path,allow_pickle=True)[i%acts_per_file][layer]).to(device) if 'mlp' in args.using_act or 'layer' in args.using_act else torch.from_numpy(np.load(file_path,allow_pickle=True)[idx%acts_per_file][layer][head*128:(head*128)+128]).to(device)
+                inputs = torch.stack(acts,axis=0)
+                if 'unitnorm' in args.probes_file_name or 'individual_linear_orthogonal' in args.probes_file_name or 'individual_linear_specialised' in args.probes_file_name: inputs = inputs / inputs.pow(2).sum(dim=-1).sqrt().unsqueeze(-1) # unit normalise
+                preds = torch.sigmoid(linear_model(inputs).data)
+                alltokens_preds.append(preds.cpu().numpy())
+        np.save(f'{args.save_path}/probes/{args.probes_file_name}_{args.responses_file_name}_alltokens_preds.npy',alltokens_preds)
 
 
     all_val_pred, all_val_true = np.load(f'{args.save_path}/probes/{args.probes_file_name}_val_pred.npy'), np.load(f'{args.save_path}/probes/{args.probes_file_name}_val_true.npy')
@@ -257,9 +289,9 @@ def main():
             # vote_distri = np.array([cls1_vote, 1 - cls1_vote])
             # mc5_entropy = (-vote_distri*np.nan_to_num(np.log2(vote_distri),neginf=0)).sum()
             
-            # sample_pred_chosen = sample_pred[best_probe_idxs][1]
-            sample_pred_chosen = np.squeeze(all_logits[:,i,:])[best_probe_idxs]*100
-            sample_pred_chosen[sample_pred_chosen<0] = 0
+            sample_pred_chosen = sample_pred[best_probe_idxs][1]
+            # sample_pred_chosen = np.squeeze(all_logits[:,i,:])[best_probe_idxs]*100
+            # sample_pred_chosen[sample_pred_chosen<0] = 0
             sample_pred_chosen = np.exp(sample_pred_chosen)/sum(np.exp(sample_pred_chosen))
             mc5_entropy = (-sample_pred_chosen*np.nan_to_num(np.emath.logn(5, sample_pred_chosen),neginf=0)).sum()
             if labels[i]==hallu_cls: mc5_entropy_hallu.append(mc5_entropy)
@@ -291,6 +323,12 @@ def main():
         fig.savefig(f'{args.save_path}/plot1.png')
     
     print('\n')
+
+
+    # Visualise probe prediction pattern
+    for i,sample_preds in enumerate(alltokens_preds):
+        sns_fig = sns.heatmap(sample_preds, linewidth=0.5)
+        sns_fig.fig.savefig(f'{args.save_path}/predplot{i}.png')
 
 
     # Find most confident layers
