5aae6620d661fc5cf6f9994e4cb99ab3bb07a306
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-05-28 20:26:22 +0100

    upd

--- analyse_crossdataset_bce.py
+++ analyse_crossdataset_bce.py
@@ -301,43 +301,43 @@ def main():
         # alltokens_preds = np.load(f'{args.save_path}/probes/{args.probes_file_name}_{args.responses_file_name}_alltokens_preds.npy')
         raise FileNotFoundError
     except FileNotFoundError:
-        # alltokens_preds = []
-        # # Get predictions from probes trained on greedy/baseline responses
-        # args.using_act = 'layer' if 'layer' in args.probes_file_name else 'mlp'
-        # args.token = 'all'
-        # act_dims = {'mlp':4096,'mlp_l1':11008,'ah':128,'layer':4096}
-        # bias = False if 'no_bias' in args.probes_file_name else True
-        # head = 0
-        # kld_probe = 0
+        alltokens_preds = []
+        # Get predictions from probes trained on greedy/baseline responses
+        args.using_act = 'layer' if 'layer' in args.probes_file_name else 'mlp'
+        args.token = 'all'
+        act_dims = {'mlp':4096,'mlp_l1':11008,'ah':128,'layer':4096}
+        bias = False if 'no_bias' in args.probes_file_name else True
+        head = 0
+        kld_probe = 0
         for i in tqdm(samples_neg_affected[:10] + samples_pos_affected[:10]):
-        #     # Load activations
-        #     act_type = {'mlp':'mlp_wise','mlp_l1':'mlp_l1','ah':'head_wise','layer':'layer_wise'}
-        #     file_end = i-(i%acts_per_file)+acts_per_file # 487: 487-(87)+100
-        #     file_path = f'{args.save_path}/features/{args.model_name}_{args.dataset_name}_{args.token}/{args.model_name}_{args.dataset_name}_{args.responses_file_name}_{args.token}_{act_type[args.using_act]}_{file_end}.pkl'
-        #     acts_by_layer = torch.from_numpy(np.load(file_path,allow_pickle=True)[i%acts_per_file]).to(device) if 'mlp' in args.using_act or 'layer' in args.using_act else None # torch.from_numpy(np.load(file_path,allow_pickle=True)[idx%acts_per_file][layer][head*128:(head*128)+128]).to(device)
-        #     preds_by_layer = []
-        #     for layer in range(num_layers):
-        #         # Load model
-        #         try:
-        #             linear_model = torch.load(f'{args.save_path}/probes/models/{args.probes_file_name}_model0_{layer}_{head}_{kld_probe}')
-        #         except FileNotFoundError:
-        #             linear_model = torch.load(f'{args.save_path}/probes/models/{args.probes_file_name}_model0_{layer}_{head}')
-        #         linear_model.eval()
-        #         inputs = acts_by_layer[layer][resp_start_idxs[i]:]
-        #         if 'unitnorm' in args.probes_file_name or 'individual_linear_orthogonal' in args.probes_file_name or 'individual_linear_specialised' in args.probes_file_name or ('individual_linear' in args.probes_file_name and 'no_bias' in args.probes_file_name):
-        #             inputs = inputs / inputs.pow(2).sum(dim=-1).sqrt().unsqueeze(-1) # unit normalise
-        #         preds = torch.sigmoid(linear_model(inputs).data).cpu().numpy()
-        #         preds_by_layer.append(np.array([pred-layer_pred_thresholds[layer] for pred in preds]))
-        #     preds_by_layer = np.stack(preds_by_layer)
-        #     alltokens_preds.append(preds_by_layer)
-            print(i,responses[i])
+            # Load activations
+            act_type = {'mlp':'mlp_wise','mlp_l1':'mlp_l1','ah':'head_wise','layer':'layer_wise'}
+            file_end = i-(i%acts_per_file)+acts_per_file # 487: 487-(87)+100
+            file_path = f'{args.save_path}/features/{args.model_name}_{args.dataset_name}_{args.token}/{args.model_name}_{args.dataset_name}_{args.responses_file_name}_{args.token}_{act_type[args.using_act]}_{file_end}.pkl'
+            acts_by_layer = torch.from_numpy(np.load(file_path,allow_pickle=True)[i%acts_per_file]).to(device) if 'mlp' in args.using_act or 'layer' in args.using_act else None # torch.from_numpy(np.load(file_path,allow_pickle=True)[idx%acts_per_file][layer][head*128:(head*128)+128]).to(device)
+            preds_by_layer = []
+            for layer in range(num_layers):
+                # Load model
+                try:
+                    linear_model = torch.load(f'{args.save_path}/probes/models/{args.probes_file_name}_model0_{layer}_{head}_{kld_probe}')
+                except FileNotFoundError:
+                    linear_model = torch.load(f'{args.save_path}/probes/models/{args.probes_file_name}_model0_{layer}_{head}')
+                linear_model.eval()
+                inputs = acts_by_layer[layer][resp_start_idxs[i]:]
+                if 'unitnorm' in args.probes_file_name or 'individual_linear_orthogonal' in args.probes_file_name or 'individual_linear_specialised' in args.probes_file_name or ('individual_linear' in args.probes_file_name and 'no_bias' in args.probes_file_name):
+                    inputs = inputs / inputs.pow(2).sum(dim=-1).sqrt().unsqueeze(-1) # unit normalise
+                preds = torch.sigmoid(linear_model(inputs).data).cpu().numpy()
+                preds_by_layer.append(np.array([pred-layer_pred_thresholds[layer] for pred in preds]))
+            preds_by_layer = np.stack(preds_by_layer)
+            alltokens_preds.append(preds_by_layer)
+            # print(i,responses[i])
         # np.save(f'{args.save_path}/probes/{args.probes_file_name}_{args.responses_file_name}_alltokens_preds.npy',alltokens_preds)
 
-    # # Visualise probe prediction pattern
-    # for i,sample_preds in tqdm(enumerate(alltokens_preds)):
-    #     fig, axs = plt.subplots(1,1)
-    #     sns_fig = sns.heatmap(np.squeeze(sample_preds), linewidth=0.5)
-    #     sns_fig.get_figure().savefig(f'{args.save_path}/predplot{i}.png')
+    # Visualise probe prediction pattern
+    for i,sample_preds in tqdm(enumerate(alltokens_preds)):
+        fig, axs = plt.subplots(1,1)
+        sns_fig = sns.heatmap(np.squeeze(sample_preds), linewidth=0.5)
+        sns_fig.get_figure().savefig(f'{args.save_path}/predplot{i}.png')
 
 
     # Find most confident layers
