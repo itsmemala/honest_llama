9a05165f2b36082e2dc10a1d29aed933f8bcaaf3
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-05-28 17:27:43 +0100

    Update analyse_crossdataset_bce.py

--- analyse_crossdataset_bce.py
+++ analyse_crossdataset_bce.py
@@ -136,25 +136,24 @@ def main():
         # Get predictions from probes trained on greedy/baseline responses
         args.using_act = 'layer' if 'layer' in args.probes_file_name else 'mlp'
         args.token = 'all'
-        for layer in tqdm(range(num_layers)):
-            # Load model
-            act_dims = {'mlp':4096,'mlp_l1':11008,'ah':128,'layer':4096}
-            bias = False if 'no_bias' in args.probes_file_name else True
-            head = 0
-            kld_probe = 0
-            try:
-                linear_model = torch.load(f'{args.save_path}/probes/models/{args.probes_file_name}_model0_{layer}_{head}_{kld_probe}')
-            except FileNotFoundError:
-                linear_model = torch.load(f'{args.save_path}/probes/models/{args.probes_file_name}_model0_{layer}_{head}')
-            linear_model.eval()
+        act_dims = {'mlp':4096,'mlp_l1':11008,'ah':128,'layer':4096}
+        bias = False if 'no_bias' in args.probes_file_name else True
+        head = 0
+        kld_probe = 0
+        for i in tqdm(samples_neg_affected[:2] + samples_pos_affected[:2]):
             # Load activations
-            acts = []
-            # print(samples_neg_affected[:10] + samples_pos_affected[:10])
-            for i in samples_neg_affected[:2] + samples_pos_affected[:2]:
-                act_type = {'mlp':'mlp_wise','mlp_l1':'mlp_l1','ah':'head_wise','layer':'layer_wise'}
-                file_end = i-(i%acts_per_file)+acts_per_file # 487: 487-(87)+100
-                file_path = f'{args.save_path}/features/{args.model_name}_{args.dataset_name}_{args.token}/{args.model_name}_{args.dataset_name}_{args.responses_file_name}_{args.token}_{act_type[args.using_act]}_{file_end}.pkl'
-                inputs = torch.from_numpy(np.load(file_path,allow_pickle=True)[i%acts_per_file][layer]).to(device) if 'mlp' in args.using_act or 'layer' in args.using_act else None # torch.from_numpy(np.load(file_path,allow_pickle=True)[idx%acts_per_file][layer][head*128:(head*128)+128]).to(device)
+            act_type = {'mlp':'mlp_wise','mlp_l1':'mlp_l1','ah':'head_wise','layer':'layer_wise'}
+            file_end = i-(i%acts_per_file)+acts_per_file # 487: 487-(87)+100
+            file_path = f'{args.save_path}/features/{args.model_name}_{args.dataset_name}_{args.token}/{args.model_name}_{args.dataset_name}_{args.responses_file_name}_{args.token}_{act_type[args.using_act]}_{file_end}.pkl'
+            acts_by_layer = torch.from_numpy(np.load(file_path,allow_pickle=True)[i%acts_per_file]).to(device) if 'mlp' in args.using_act or 'layer' in args.using_act else None # torch.from_numpy(np.load(file_path,allow_pickle=True)[idx%acts_per_file][layer][head*128:(head*128)+128]).to(device)
+            for layer in range(num_layers):
+                # Load model
+                try:
+                    linear_model = torch.load(f'{args.save_path}/probes/models/{args.probes_file_name}_model0_{layer}_{head}_{kld_probe}')
+                except FileNotFoundError:
+                    linear_model = torch.load(f'{args.save_path}/probes/models/{args.probes_file_name}_model0_{layer}_{head}')
+                linear_model.eval()
+                inputs = acts_by_layer[layer]
                 if 'unitnorm' in args.probes_file_name or 'individual_linear_orthogonal' in args.probes_file_name or 'individual_linear_specialised' in args.probes_file_name or ('individual_linear' in args.probes_file_name and 'no_bias' in args.probes_file_name):
                     inputs = inputs / inputs.pow(2).sum(dim=-1).sqrt().unsqueeze(-1) # unit normalise
                 preds = torch.sigmoid(linear_model(inputs).data)
@@ -328,8 +327,7 @@ def main():
 
 
     # Visualise probe prediction pattern
-    for i in enumerate(alltokens_preds.shape[1]):
-        sample_preds = alltokens_preds[:,i,:,:]
+    for sample_preds in tqdm(alltokens_preds):
         sns_fig = sns.heatmap(sample_preds, linewidth=0.5)
         sns_fig.fig.savefig(f'{args.save_path}/predplot{i}.png')
 
