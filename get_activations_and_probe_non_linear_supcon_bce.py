4b8c89742fc79463f58f7b16756d1096c28028b0
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-06-04 13:34:03 +0100

    Update get_activations_and_probe_non_linear_supcon_bce.py

--- get_activations_and_probe_non_linear_supcon_bce.py
+++ get_activations_and_probe_non_linear_supcon_bce.py
@@ -564,9 +564,9 @@ def main():
                     print('Similarity of knn samples at current layer:',sim[top_k])
                     print('Indices of knn samples at current layer:',cur_knn_idxs)
                     top_k_val = torch.min(torch.topk(sim,knn_k)[0][torch.topk(sim,knn_k)[0]>0]).item() # get smallest top-k sim val (only pos)
-                    print((sim>top_k_val)[:5])
+                    print(torch.sum(sim>top_k_val))
                     print((sim>top_k_val)[:5].nonzero(as_tuple=True))
-                    top_k = (sim>top_k_val).nonzero(as_tuple=True).detach().cpu().numpy() # save indices of top k similar vectors
+                    top_k = (sim>top_k_val).nonzero(as_tuple=True)[0].detach().cpu().numpy() # save indices of top k similar vectors
                     cur_knn_idxs = np.array(hallu_idxs)[top_k]
                     model_wise_mc_sample_idxs.append(cur_knn_idxs)
                     print('Number of top-k samples:',len(cur_knn_idxs))
