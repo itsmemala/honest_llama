bdb3d73442cd4d81df6507b047f6c8eb58796bb3
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-06-25 13:40:44 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -51,7 +51,7 @@ class My_Transformer_Layer(torch.nn.Module):
     # build the constructor
     def __init__(self, n_inputs, n_layers, n_outputs, bias):
         super().__init__()
-        d_model = 64 # 256
+        d_model = 128 # 256
         dim_feedforward = 1024 # 256
         nhead = 16 # 16 # 8
         self.linear = torch.nn.Linear(n_inputs, d_model, bias)
