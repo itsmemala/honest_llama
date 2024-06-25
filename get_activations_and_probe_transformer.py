786ad0b6795ba27f6e44136d7134f3c4d7e8078e
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-06-25 13:15:11 +0100

    Update get_activations_and_probe_transformer.py

--- get_activations_and_probe_transformer.py
+++ get_activations_and_probe_transformer.py
@@ -310,7 +310,7 @@ def main():
                         file_end = idx-(idx%args.acts_per_file)+args.acts_per_file # 487: 487-(87)+100
                         file_path = f'{args.save_path}/features/{args.model_name}_{args.dataset_name}_{args.token}/{args.model_name}_{args.train_file_name}_{args.token}_{act_type[args.using_act]}_{file_end}.pkl'
                         act = torch.load(file_path)[idx%args.acts_per_file].to(device)
-                        if act.shape[1] > 12: continue # Skip inputs with large number of tokens to avoid OOM
+                        if act.shape[1] > 20: continue # Skip inputs with large number of tokens to avoid OOM
                         act = torch.reshape(act, (act.shape[0]*act.shape[1],act.shape[2])) # (layers,tokens,act_dims) -> (layers*tokens,act_dims)
                         batch_target_idxs.append(k)
                     else:
