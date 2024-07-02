4d7a1036fe253d33fc1c7f97d5e436ac5ba397b0
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-07-03 00:19:50 +0100

    Update get_activations_and_probe_transformer.py

--- get_activations_and_probe_transformer.py
+++ get_activations_and_probe_transformer.py
@@ -316,8 +316,8 @@ def main():
                     if 'tagged_tokens' in args.token: 
                         file_end = idx-(idx%args.acts_per_file)+args.acts_per_file # 487: 487-(87)+100
                         file_path = f'{args.save_path}/features/{args.model_name}_{args.dataset_name}_{args.token}/{args.model_name}_{args.train_file_name}_{args.token}_{act_type[args.using_act]}_{file_end}.pkl'
-                        # act = torch.load(file_path)[idx%args.acts_per_file].to(device)
-                        act = torch.from_numpy(np.load(file_path,allow_pickle=True)[idx%args.acts_per_file]).to(device)
+                        act = torch.load(file_path)[idx%args.acts_per_file].to(device)
+                        # act = torch.from_numpy(np.load(file_path,allow_pickle=True)[idx%args.acts_per_file]).to(device)
                         if act.shape[1] > args.max_tokens: continue # Skip inputs with large number of tokens to avoid OOM
                         if args.tokens_first: act = torch.swapaxes(act, 0, 1) # (layers,tokens,act_dims) -> (tokens,layers,act_dims)
                         sep_token = torch.zeros(act.shape[0],1,act.shape[2]).to(device)
@@ -354,8 +354,8 @@ def main():
                     if 'tagged_tokens' in args.token: 
                         file_end = idx-(idx%args.acts_per_file)+args.acts_per_file # 487: 487-(87)+100
                         file_path = f'{args.save_path}/features/{args.model_name}_{args.dataset_name}_{args.token}/{args.model_name}_{args.train_file_name}_{args.token}_{act_type[args.using_act]}_{file_end}.pkl'
-                        # act = torch.load(file_path)[idx%args.acts_per_file].to(device)
-                        act = torch.from_numpy(np.load(file_path,allow_pickle=True)[idx%args.acts_per_file]).to(device)
+                        act = torch.load(file_path)[idx%args.acts_per_file].to(device)
+                        # act = torch.from_numpy(np.load(file_path,allow_pickle=True)[idx%args.acts_per_file]).to(device)
                         if act.shape[1] > args.max_tokens: continue # Skip inputs with large number of tokens to avoid OOM
                         if args.tokens_first: act = torch.swapaxes(act, 0, 1) # (layers,tokens,act_dims) -> (tokens,layers,act_dims)
                         sep_token = torch.zeros(act.shape[0],1,act.shape[2]).to(device)
@@ -412,8 +412,8 @@ def main():
                     if 'tagged_tokens' in args.token: 
                         file_end = idx-(idx%args.acts_per_file)+args.acts_per_file # 487: 487-(87)+100
                         file_path = f'{args.save_path}/features/{args.model_name}_{args.dataset_name}_{args.token}/{args.model_name}_{args.train_file_name}_{args.token}_{act_type[args.using_act]}_{file_end}.pkl'
-                        # act = torch.load(file_path)[idx%args.acts_per_file].to(device)
-                        act = torch.from_numpy(np.load(file_path,allow_pickle=True)[idx%args.acts_per_file]).to(device)
+                        act = torch.load(file_path)[idx%args.acts_per_file].to(device)
+                        # act = torch.from_numpy(np.load(file_path,allow_pickle=True)[idx%args.acts_per_file]).to(device)
                         if act.shape[1] > args.max_tokens: continue # Skip inputs with large number of tokens to avoid OOM
                         if args.tokens_first: act = torch.swapaxes(act, 0, 1) # (layers,tokens,act_dims) -> (tokens,layers,act_dims)
                         sep_token = torch.zeros(act.shape[0],1,act.shape[2]).to(device)
@@ -455,8 +455,8 @@ def main():
                         if 'tagged_tokens' in args.token: 
                             file_end = idx-(idx%args.acts_per_file)+args.acts_per_file # 487: 487-(87)+100
                             file_path = f'{args.save_path}/features/{args.model_name}_{args.dataset_name}_{args.token}/{args.model_name}_{args.test_file_name}_{args.token}_{act_type[args.using_act]}_{file_end}.pkl'
-                            # act = torch.load(file_path)[idx%args.acts_per_file].to(device)
-                            act = torch.from_numpy(np.load(file_path,allow_pickle=True)[idx%args.acts_per_file]).to(device)
+                            act = torch.load(file_path)[idx%args.acts_per_file].to(device)
+                            # act = torch.from_numpy(np.load(file_path,allow_pickle=True)[idx%args.acts_per_file]).to(device)
                             if act.shape[1] > args.max_tokens: continue # Skip inputs with large number of tokens to avoid OOM
                             if args.tokens_first: act = torch.swapaxes(act, 0, 1) # (layers,tokens,act_dims) -> (tokens,layers,act_dims)
                             sep_token = torch.zeros(act.shape[0],1,act.shape[2]).to(device)
