1e3eba6e971b08b2099fd01bf82f40ef2ea927d8
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-07-02 15:29:41 +0100

    Update get_activations.py

--- get_activations.py
+++ get_activations.py
@@ -133,8 +133,8 @@ def main():
     # else: 
     #     prompts, labels = formatter(dataset, tokenizer)
 
-    if args.token=='tagged_tokens' or args.token=='tagged_tokens_and_last':
-        tagged_token_idxs = get_token_tags(prompts,prompt_tokens)
+    # if args.token=='tagged_tokens' or args.token=='tagged_tokens_and_last':
+    tagged_token_idxs = get_token_tags(prompts,prompt_tokens)
 
 
     if 'tqa' in args.dataset_name:
