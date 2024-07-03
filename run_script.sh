f09b07eca490161434cd92051e241cc97695da6d
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-07-04 00:53:24 +0100

    Update run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -24,12 +24,12 @@
 # python get_activations.py alpaca_7B trivia_qa --mlp_l1 Yes --token answer_last --file_name trivia_qa_greedy_responses_train5000 --save_path ~/Desktop/honest_llama_data
 # python get_activations.py alpaca_7B trivia_qa --mlp_l1 Yes --token prompt_last --file_name trivia_qa_greedy_responses_train5000 --save_path ~/Desktop/honest_llama_data
 # python get_activations.py alpaca_7B trivia_qa --mlp_l1 Yes --token maxpool_all --file_name trivia_qa_greedy_responses_train5000 --save_path ~/Desktop/honest_llama_data
-python get_activations.py alpaca_7B trivia_qa --token answer_last --file_name trivia_qa_greedy_responses_validation1800 --save_path ~/Desktop/honest_llama_data
-python get_activations.py alpaca_7B trivia_qa --token prompt_last --file_name trivia_qa_greedy_responses_validation1800 --save_path ~/Desktop/honest_llama_data
-python get_activations.py alpaca_7B trivia_qa --token maxpool_all --file_name trivia_qa_greedy_responses_validation1800 --save_path ~/Desktop/honest_llama_data
-python get_activations.py alpaca_7B trivia_qa --mlp_l1 Yes --token answer_last --file_name trivia_qa_greedy_responses_validation1800 --save_path ~/Desktop/honest_llama_data
-python get_activations.py alpaca_7B trivia_qa --mlp_l1 Yes --token prompt_last --file_name trivia_qa_greedy_responses_validation1800 --save_path ~/Desktop/honest_llama_data
-python get_activations.py alpaca_7B trivia_qa --mlp_l1 Yes --token maxpool_all --file_name trivia_qa_greedy_responses_validation1800 --save_path ~/Desktop/honest_llama_data
+# python get_activations.py alpaca_7B trivia_qa --token answer_last --file_name trivia_qa_greedy_responses_validation1800 --save_path ~/Desktop/honest_llama_data
+# python get_activations.py alpaca_7B trivia_qa --token prompt_last --file_name trivia_qa_greedy_responses_validation1800 --save_path ~/Desktop/honest_llama_data
+# python get_activations.py alpaca_7B trivia_qa --token maxpool_all --file_name trivia_qa_greedy_responses_validation1800 --save_path ~/Desktop/honest_llama_data
+# python get_activations.py alpaca_7B trivia_qa --mlp_l1 Yes --token answer_last --file_name trivia_qa_greedy_responses_validation1800 --save_path ~/Desktop/honest_llama_data
+# python get_activations.py alpaca_7B trivia_qa --mlp_l1 Yes --token prompt_last --file_name trivia_qa_greedy_responses_validation1800 --save_path ~/Desktop/honest_llama_data
+# python get_activations.py alpaca_7B trivia_qa --mlp_l1 Yes --token maxpool_all --file_name trivia_qa_greedy_responses_validation1800 --save_path ~/Desktop/honest_llama_data
 
 # python get_activations.py alpaca_7B nq_open --token answer_last --file_name nq_open_greedy_responses_train5000 --save_path ~/Desktop/honest_llama_data
 # python get_activations.py alpaca_7B nq_open --token prompt_last --file_name nq_open_greedy_responses_train5000 --save_path ~/Desktop/honest_llama_data
@@ -46,3 +46,9 @@ python get_activations.py alpaca_7B trivia_qa --mlp_l1 Yes --token maxpool_all -
 
 # python get_activations.py llama_7B nq_open --token answer_last --file_name nq_open_greedy_responses_1800 --save_path ~/Desktop/honest_llama_data
 # python get_activations.py llama_7B nq_open --token answer_last --file_name nq_open_greedy_responses_train5000 --save_path ~/Desktop/honest_llama_data
+
+
+python get_activations.py alpaca_7B trivia_qa --token tagged_tokens_and_last --file_name trivia_qa_greedy_responses_train5000  --device 0 --save_path ~/Desktop/honest_llama_data
+python get_activations.py alpaca_7B trivia_qa --token tagged_tokens_and_last --file_name trivia_qa_greedy_responses_validation1800  --device 0 --save_path ~/Desktop/honest_llama_data
+
+python get_activations_and_probe_transformer.py alpaca_7B trivia_qa --train_file_name trivia_qa_greedy_responses_train5000 --train_labels_file_name trivia_qa_greedy_responses_labels_train5000 --test_file_name trivia_qa_greedy_responses_validation1800 --test_labels_file_name trivia_qa_greedy_responses_labels_validation1800 --len_dataset 5000 --num_folds 1 --using_act layer --token tagged_tokens --method transformer_hallu_pos --bs 32 --epochs 10 --lr 0.00001 --save_probes True --device 0 --save_path ~/Desktop/honest_llama_data --max_tokens 40
