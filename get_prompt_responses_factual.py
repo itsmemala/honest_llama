fd0df9f7b40a7f22ab20cec291bc381b3bd42045
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-07-03 16:25:57 +0100

    Update get_prompt_responses_factual.py

--- get_prompt_responses_factual.py
+++ get_prompt_responses_factual.py
@@ -95,6 +95,7 @@ def main():
 
     print('Loading model..')
     tokenizer = llama.LlamaTokenizer.from_pretrained(MODEL)
+    if args.num_ret_seq>1 and args.model_name=='alpaca_7B': tokenizer.pad_token = tokenizer.eos_token
     model = llama.LlamaForCausalLM.from_pretrained(MODEL, low_cpu_mem_usage=True, torch_dtype=torch.float16, device_map="auto")
     if args.num_ret_seq>1 and args.model_name=='llama_2_7B': model = model.bfloat16() # Numerical instability; Solution from: https://github.com/meta-llama/llama/issues/380
     device = "cuda"
