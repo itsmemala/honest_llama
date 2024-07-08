d73269ccdbe0208d8ddb8b9f66b7bb4e311bb7b6
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-07-08 21:12:25 +0100

    Update run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -44,9 +44,5 @@
 # python get_activations.py alpaca_7B nq_open --mlp_l1 Yes --token prompt_last --file_name nq_open_greedy_responses_1800 --save_path ~/Desktop/honest_llama_data
 # python get_activations.py alpaca_7B nq_open --mlp_l1 Yes --token maxpool_all --file_name nq_open_greedy_responses_1800 --save_path ~/Desktop/honest_llama_data
 
-python get_activations.py hl_llama_7B strqa --token least_likely --file_name strqa_baseline_responses_train  --device 0 --save_path ~/Desktop/honest_llama_data
-python get_activations.py hl_llama_7B strqa --token least_likely --file_name strqa_baseline_responses_test  --device 0 --save_path ~/Desktop/honest_llama_data
-python get_activations.py hl_llama_7B strqa --token tagged_tokens_and_last --file_name strqa_baseline_responses_train  --device 0 --save_path ~/Desktop/honest_llama_data
-python get_activations.py hl_llama_7B strqa --token tagged_tokens_and_last --file_name strqa_baseline_responses_test  --device 0 --save_path ~/Desktop/honest_llama_data
-
-python get_activations_and_probe_transformer.py hl_llama_7B strqa --train_file_name strqa_baseline_responses_train --test_file_name strqa_baseline_responses_test --len_dataset 1832 --num_folds 1 --using_act layer --token tagged_tokens_and_last --method transformer_hallu_pos --bs 8 --epochs 10 --lr 0.00001 --save_probes True --device 0 --save_path ~/Desktop/honest_llama_data --max_tokens 100
+python get_activations.py alpaca_7B nq_open --token tagged_tokens_and_last --file_name nq_open_greedy_responses_train5000  --device 0 --save_path ~/Desktop/honest_llama_data
+python get_activations.py alpaca_7B nq_open --token tagged_tokens_and_last --file_name nq_open_greedy_responses_validation1800  --device 0 --save_path ~/Desktop/honest_llama_data
