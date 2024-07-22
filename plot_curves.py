f6bc2050c2fdad88c9d4c1b60c3530c99f0c480d
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-07-22 13:06:56 +0100

    upd

--- plot_curves.py
+++ plot_curves.py
@@ -15,39 +15,56 @@ def list_of_ints(arg):
 def main():
 
     parser = argparse.ArgumentParser()
+    parser.add_argument('model_name', type=str, default='')
+    parser.add_argument('dataset_name', type=str, default='')
     parser.add_argument('--save_path',type=str, default='')
     parser.add_argument('--probes_file_name',type=str, default='')
+    parser.add_argument('--using_act',type=str, default='')
+    parser.add_argument('--token',type=str, default='')
+    parser.add_argument('--method',type=str, default='')
+    parser.add_argument('--bs',type=int, default=None)
+    parser.add_argument('--lr',type=float, default=None)
     args = parser.parse_args()
 
     device = 0
 
-    wandb.init(
-    project="LLM-Hallu-Detection",
-    config={
-    "run_name": args.probes_file_name
-    }
-    )
+    # wandb.init(
+    # project="LLM-Hallu-Detection",
+    # config={
+    # "run_name": args.probes_file_name,
+    # "model": args.model_name,
+    # "dataset": args.dataset_name,
+    # "act_type": args.using_act,
+    # "token": args.token,
+    # "method": args.method,
+    # "bs": args.bs,
+    # "lr": args.lr,
+    # }
+    # )
 
     val_loss = np.load(f'{args.save_path}/probes/{args.probes_file_name}_val_loss.npy', allow_pickle=True).item()
+    print(val_loss.shape)
     train_loss = np.load(f'{args.save_path}/probes/{args.probes_file_name}_train_loss.npy', allow_pickle=True).item()
+    print(train_loss.shape)
     try:
         supcon_train_loss = np.load(f'{args.save_path}/probes/{args.probes_file_name}_supcon_train_loss.npy', allow_pickle=True).item()
+        print(supcon_train_loss.shape)
     except FileNotFoundError:
         supcon_train_loss = []
     
-    plt.subplot(1, 3, 1)
-    plt.plot(val_loss)
-    plt.xlabel("epoch")
-    plt.ylabel( "loss")
-    plt.subplot(1, 3, 2)
-    plt.plot(train_loss)
-    plt.xlabel("epoch")
-    plt.ylabel( "loss")
-    plt.subplot(1, 3, 3)
-    plt.plot(supcon_train_loss)
-    plt.xlabel("epoch")
-    plt.ylabel( "loss")
-    wandb.log({'chart': plt})
+    # plt.subplot(1, 3, 1)
+    # plt.plot(val_loss)
+    # plt.xlabel("epoch")
+    # plt.ylabel( "loss")
+    # plt.subplot(1, 3, 2)
+    # plt.plot(train_loss)
+    # plt.xlabel("epoch")
+    # plt.ylabel( "loss")
+    # plt.subplot(1, 3, 3)
+    # plt.plot(supcon_train_loss)
+    # plt.xlabel("epoch")
+    # plt.ylabel( "loss")
+    # wandb.log({'chart': plt})
 
 
     
