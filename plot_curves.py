a9a60ac25963fa629adb6a3b4f513c295c9cf403
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-07-25 15:18:07 +0100

    Update plot_curves.py

--- plot_curves.py
+++ plot_curves.py
@@ -71,9 +71,9 @@ def main():
     plt.plot(val_loss, label='val_ce_loss')
     plt.plot(train_loss, label='train_ce_loss')
     plt.plot(supcon_train_loss, label='train_supcon_loss')
+    plt.legend(loc="upper left")
     plt.subplot(1, 2, 2)
     plt.plot(val_auc, label='val_auc')
-    plt.legend(loc="upper left")
     plt.savefig(f'{args.save_path}/testfig.png')
 
     # wandb.init(
