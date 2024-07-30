3b389cc42c62ed95a0d5f40ec6698ed6916ff683
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-07-30 22:34:45 +0100

    Update run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -2,11 +2,7 @@
 # python get_activations.py alpaca_7B nq_open --token tagged_tokens --file_name nq_open_greedy_responses_validation1800  --device 0 --save_path ~/Desktop/honest_llama_data
 # python get_activations_and_probe_transformer.py alpaca_7B nq_open --train_file_name nq_open_greedy_responses_train5000 --train_labels_file_name nq_open_greedy_responses_labels_train5000 --test_file_name nq_open_greedy_responses_validation1800 --test_labels_file_name nq_open_greedy_responses_labels_validation1800 --len_dataset 5000 --num_folds 1 --using_act layer --token tagged_tokens --method transformer_hallu_pos --bs 32 --epochs 50 --lr 0.0001 --norm_input True --save_probes True --device 0 --save_path ~/Desktop/honest_llama_data --max_tokens 40 --use_pe True
 
-source activate iti
 
-#Run your code
-
-# cd honest_llama
 #python get_activations.py hl_llama_7B strqa --token answer_last --file_name strqa_baseline_responses_train  --device 0 --save_path ~/honest_llama_data
 #python get_activations.py hl_llama_7B strqa --token answer_last --file_name strqa_baseline_responses_test  --device 0 --save_path ~/honest_llama_data
 
