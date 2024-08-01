b0905643b8278c24b3d2676fa896988079ca92ec
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-08-01 14:52:15 +0100

    Update get_activations_and_probe_non_linear_supcon_bce.py

--- get_activations_and_probe_non_linear_supcon_bce.py
+++ get_activations_and_probe_non_linear_supcon_bce.py
@@ -509,6 +509,7 @@ def main():
                             targets = torch.cat([torch.Tensor([y_label for j in range(activations[b_idx].shape[0])]) for b_idx,(idx,y_label) in enumerate(zip(batch['inputs_idxs'],batch['labels']))],dim=0).type(torch.LongTensor)
                         # if 'individual_linear_orthogonal' in args.method or 'individual_linear_specialised' in args.method or ('individual_linear' in args.method and args.no_bias) or args.norm_input: inputs = inputs / inputs.pow(2).sum(dim=1).sqrt().unsqueeze(-1) # unit normalise
                         if args.norm_input: inputs = (inputs - torch.mean(inputs, dim=-1).unsqueeze(-1))/torch.std(inputs, dim=-1).unsqueeze(-1) # mean normalise
+                        print(torch.mean(inputs, dim=-1).sum()==0)
                         assert torch.mean(inputs, dim=-1).sum()==0
                         if 'supcon' in args.method:
                             # SupCon backward
