19c80da8f825d54b7fcc16c7529c01908c241b12
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-08-02 15:37:02 +0100

    upd

---
+++ analyse_rankings_counselling.py
@@ -0,0 +1,54 @@
+import os
+import numpy as np
+import json
+import pandas as pd
+from scipy.stats import rankdata
+import argparse
+
+def boolean_string(s):
+    if s not in {'False', 'True'}:
+        raise ValueError('Not a valid boolean string')
+    return s == 'True'
+
+def main(): 
+    """
+    
+    """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--save_path',type=str, default='')
+    args = parser.parse_args()
+
+
+    llama_token_logprobs = torch.from_numpy(np.load(f'{args.save_path}/features/counselling_wudata_token_logprobs.pkl',allow_pickle=True))
+    gpt_token_logprobs_df = pd.read_excel(f'{args.save_path}/features/token_logprobs.xlsx')
+    sampled_responses = pd.read_excel(file_path+'annotations_filtered.xlsx', index_col=0, sheet_name='Sheet2')
+
+    groundtruth = []
+    for context_id in sampled_responses.index:
+        context_truth = []
+        for resp_id in [1,2,3,4,5,6,7,8,9]:
+            if sampled_responses['annotation'+str(resp_id)][context_id]=='Good':
+                context_truth.append('Resp'+str(resp_id))
+        groundtruth.append(context_truth)
+    
+    llama_rankings, gpt_rankings = [], []
+    for i,row in gpt_token_logprobs_df.iterrows():
+        llama_scores, gpt_scores = [], []
+        llama_iterator = 0
+        for resp_id in ['Resp1','Resp2','Resp3','Resp4','Resp5','Resp6','Resp7','Resp8','Resp9']:
+            try:
+                gpt_scores.append(np.mean(row[resp_id]))
+                llama_scores.append(llama_token_logprobs[llama_iterator])
+                llama_iterator += 1
+            except TypeError: # Some prompts have fewer samples
+                continue
+        # rankdata(llama_scores)
+        # rankdata(gpt_scores)
+        # break
+    
+    assert len(llama_token_logprobs)==llama_iterator
+
+
+if __name__ == '__main__':
+    main()
--- get_activations_and_probe_transformer.py
+++ get_activations_and_probe_transformer.py
@@ -166,16 +166,33 @@ def main():
         file_path = f'{args.save_path}/responses/{args.model_name}_{args.train_file_name}.json'
         prompts = tokenized_mi(file_path, tokenizer)
     elif args.dataset_name == 'gsm8k' or args.dataset_name == 'strqa' or ('baseline' in args.train_file_name or 'dola' in args.train_file_name):
+        num_samples = args.num_samples if ('sampled' in args.train_file_name and args.num_samples is not None) else 9 if 'sampled' in args.train_file_name else 1
         file_path = f'{args.save_path}/responses/{args.model_name}_{args.train_file_name}.json'
-        prompts, tokenized_prompts, answer_token_idxes, prompt_tokens = tokenized_from_file_v2(file_path, tokenizer)
+        prompts, tokenized_prompts, answer_token_idxes, prompt_tokens = tokenized_from_file_v2(file_path, tokenizer, num_samples)
         prompts, tokenized_prompts, answer_token_idxes, prompt_tokens = prompts[:args.len_dataset], tokenized_prompts[:args.len_dataset], answer_token_idxes[:args.len_dataset], prompt_tokens[:args.len_dataset]
         labels = []
+        num_samples_with_no_var = 0
+        all_hallu_prompts, all_nh_prompts, hetero_prompts_sum = [], [], []
         with open(file_path, 'r') as read_file:
             data = json.load(read_file)
         for i in range(len(data['full_input_text'])):
-            if 'hallu_pos' not in args.method: label = 1 if data['is_correct'][i]==True else 0
-            if 'hallu_pos' in args.method: label = 0 if data['is_correct'][i]==True else 1
-            labels.append(label)
+            if 'greedy' in args.train_labels_file_name:
+                if 'hallu_pos' not in args.method: label = 1 if data['is_correct'][i]==True else 0
+                if 'hallu_pos' in args.method: label = 0 if data['is_correct'][i]==True else 1
+                labels.append(label)
+            else:
+                sum_over_samples = 0
+                for j in range(1,num_samples+1,1):
+                    if 'hallu_pos' not in args.method: label = 1 if data['is_correct'][i][j]==True else 0
+                    if 'hallu_pos' in args.method: label = 0 if data['is_correct'][i][j]==True else 1
+                    labels.append(label)
+                    sum_over_samples += label
+                if sum_over_samples==0 or sum_over_samples==num_samples: 
+                    num_samples_with_no_var += 1
+                    if sum_over_samples==num_samples: all_hallu_prompts.append(i)
+                    if sum_over_samples==0: all_nh_prompts.append(i)
+                else:
+                    hetero_prompts_sum.append(sum_over_samples)
         labels = labels[:args.len_dataset]
         if args.test_file_name is None:
             test_prompts, test_labels = [], [] # No test file
@@ -231,6 +248,10 @@ def main():
                     if 'hallu_pos' not in args.method: label = 1 if data['rouge1_to_target']>0.3 else 0 # pos class is non-hallu
                     if 'hallu_pos' in args.method: label = 0 if data['rouge1_to_target']>0.3 else 1 # pos class is hallu
                     test_labels.append(label)
+    
+    print(num_samples_with_no_var)
+    print(len(all_hallu_prompts),len(all_nh_prompts))
+    print(np.histogram(hetero_prompts_sum, bins=num_samples-1))
     
     hallu_cls = 1 if 'hallu_pos' in args.method else 0
 
@@ -440,7 +461,7 @@ def main():
                     # supcon_train_loss.append(supcon_loss.item())
                     # CE backward
                     emb = nlinear_model.forward_upto_classifier(inputs).detach()
-                    norm_emb = F.normalize(emb, p=2, dim=-1)
+                    # norm_emb = F.normalize(emb, p=2, dim=-1)
                     outputs = nlinear_model.classifier(norm_emb) # norm before passing here?
                     loss = criterion(outputs, targets.to(device).float())
                     loss.backward()
