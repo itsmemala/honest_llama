24a4f3c1029d44b10aa4da03bcd17e1956f0dd62
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-08-14 13:32:39 +0100

    upd

--- get_baselines.py
+++ get_baselines.py
@@ -126,13 +126,9 @@ def main():
         # class_1_perc = sum([test_labels[i] for i in test_idxs])/tot
         # print('majority class:',1 if class_1_perc>0.5 else 0,'(',class_1_perc,')')
         tot = len(train_idxs)
-        print('train accuracy:',max(sum([train_labels[i] for i in train_idxs])
-                                  ,tot-sum([train_labels[i] for i in train_idxs])
-                                  )/tot)
+        print('train accuracy:',sum([train_labels[i] for i in train_idxs])/tot
         tot = len(test_idxs)
-        print('test accuracy:',max(sum([test_labels[i] for i in test_idxs])
-                                  ,tot-sum([test_labels[i] for i in test_idxs])
-                                  )/tot)
+        print('test accuracy:',sum([test_labels[i] for i in test_idxs])/tot
         # print('baseline f1:',f1_score([test_labels[i] for i in test_idxs],[1 for i in test_idxs]),f1_score([test_labels[i] for i in test_idxs],[1 for i in test_idxs],pos_label=0))
 
         print('\nUncertainty Baselines:')
