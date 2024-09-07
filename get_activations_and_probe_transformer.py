73c1c5dc391273b3902937289bd63d54067be5b0
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-09-07 23:10:58 +0100

    Update get_activations_and_probe_transformer.py

--- get_activations_and_probe_transformer.py
+++ get_activations_and_probe_transformer.py
@@ -14,7 +14,7 @@ import pickle
 import json
 from utils import get_llama_activations_bau_custom, tokenized_mi, tokenized_from_file, tokenized_from_file_v2, get_token_tags
 from utils import My_Transformer_Layer
-from losses import SupConLoss
+from .losses import SupConLoss
 from copy import deepcopy
 import llama
 import argparse
