9f845318030a4532982fd98d982caf1eb1fe7485
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-09-15 23:34:45 +0100

    Update plot_embedding_space.py

--- plot_embedding_space.py
+++ plot_embedding_space.py
@@ -225,7 +225,7 @@ def main():
     X_tsne = tsne.fit_transform(my_train_embs)
     print(tsne.kl_divergence_)
     fig, axs = plt.subplots(1,1)
-    axs[0].scatter(x=X_tsne[:, 0], y=X_tsne[:, 1], c=labels)
+    axs.scatter(x=X_tsne[:, 0], y=X_tsne[:, 1], c=labels)
     fig.savefig(f'{args.save_path}/plotemb.png')
 
 
