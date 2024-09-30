ef9f056677638fcf6fa4f86f8b7d08111427908c
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-09-30 18:01:32 +0100

    Update get_activations_and_probe_transformer.py

--- get_activations_and_probe_transformer.py
+++ get_activations_and_probe_transformer.py
@@ -90,7 +90,6 @@ def get_best_threshold(val_true, val_preds, is_knn=False):
     best_val_perf, best_t = 0, 0.5
     thresholds = np.histogram_bin_edges(val_preds, bins='auto') if is_knn else [0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95]
     print(np.histogram(val_preds, bins=thresholds))
-    sys.exit()
     for t in thresholds:
         val_pred_at_thres = deepcopy(val_preds) # Deep copy so as to not touch orig values
         if is_knn:
@@ -104,6 +103,8 @@ def get_best_threshold(val_true, val_preds, is_knn=False):
         perf = np.mean((cls1_f1,cls0_f1))
         if perf>best_val_perf:
             best_val_perf, best_t = perf, t
+    print(best_val_perf,best_t)
+    sys.exit()
     return best_t
 
 def compute_knn_dist(outputs,train_outputs,top_k=5):
