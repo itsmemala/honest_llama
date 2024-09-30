4a75ddef744aa3755c8219e27865601836b691c8
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-09-30 16:45:25 +0100

    Update get_activations_and_probe_transformer.py

--- get_activations_and_probe_transformer.py
+++ get_activations_and_probe_transformer.py
@@ -89,6 +89,8 @@ def combine_acts(idx,file_name,args):
 def get_best_threshold(val_true, val_preds, is_knn=False):
     best_val_perf, best_t = 0, 0.5
     thresholds = np.histogram_bin_edges(val_preds, bins='auto') if is_knn else [0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95]
+    print(thresholds)
+    sys.exit()
     for t in thresholds:
         val_pred_at_thres = deepcopy(val_preds) # Deep copy so as to not touch orig values
         if is_knn:
