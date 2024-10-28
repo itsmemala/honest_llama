5e3e68e7114352737fc07f94b718b4562e414c0a
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-10-28 15:36:56 +0000

    Update get_activations_and_probe_transformer.py

--- get_activations_and_probe_transformer.py
+++ get_activations_and_probe_transformer.py
@@ -977,6 +977,7 @@ def main():
                                         prior_probes_file_name = probes_file_name.replace('knn_','').replace('kmeans_','').replace(args.dist_metric+str(args.top_k)+'_','').replace(args.dist_metric+str(args.top_k)+'pca'+str(args.pca_dims)+'_','')
                                     elif args.ood_test:
                                         prior_probes_file_name = f'T{save_seed}_{args.model_name}_{args.train_file_name}_{args.len_dataset}_{args.num_folds}_{args.using_act}{args.norm_input}_{args.token}_{method_concat}_bs{args.bs}_epochs{args.epochs}_{args.lr}_{args.use_class_wgt}'
+                                        prior_probes_file_name += plot_name_concat
                                     prior_save_path = f'{args.save_path}/probes/models/{prior_probes_file_name}_model{i}'
                                     nlinear_model = torch.load(prior_save_path,map_location=device)
                                     probe_save_path = f'{args.save_path}/probes/models/{probes_file_name}_model{i}'
