f0c2a1cdc8f2bf4ecb07d0afd2fb0b544626d119
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-11-09 19:26:28 +0000

    Create plot_manual.py

---
+++ plot_manual.py
@@ -0,0 +1,20 @@
+from matplotlib import pyplot
+
+fig, axs = plt.subplots(1,1)
+paths = ['NLSC42_hl_llama_7B_trivia_qa_greedy_responses_train5000_5000_1_layerFalse_answer_last_individual_linear_hallu_pos_bs128_epochs50_0.0005_False_fpr_at_recall',
+'T42_hl_llama_7B_trivia_qa_greedy_responses_train5000_5000_1_layerFalse_answer_last_transformer_hallu_pos_bs128_epochs50_5e-05_Falseba_fpr_at_recall',
+'T42_hl_llama_7B_trivia_qa_greedy_responses_train5000_5000_1_layerFalse_answer_last_transformer_supconv2_hallu_pos_0.05_bs256_epochs500_5e-05_Falseba_fpr_at_recall_xaxis',
+'T42_hl_llama_7B_trivia_qa_sampledplus_responses_train5000_55000_1_layerFalse_answer_last_transformer_supconv2_pos_wp_hallu_pos_0.3_bs352_epochs500_5e-05_Falseba_fpr_at_recall',
+'T42_hl_llama_7B_trivia_qa_sampledplus_responses_train5000_55000_1_layerFalse_answer_last_transformer_supconv2_pos_wp_kmeans_hallu_pos_2.0_mahalanobis_centers1pca0.9_bs352_epochs500_0.0005_Falseba_bestusinglast_fpr_at_recall'
+]
+labels = ['linear_last_layer','tfmr_all_layers','tfmr_supcon','tfmr_supcon+*','tfmr_supcon+*_dist']
+for path in xpaths:
+    recall_vals = np.load(f'{path}_xaxis.npy')
+    fpr_at_recall_vals = np.load(f'{path}_yaxis.npy')
+    axs.plot(recall_vals,fpr_at_recall_vals)
+    for xy in zip(recall_vals,fpr_at_recall_vals):
+        axs.annotate('(%.2f, %.2f)' % xy, xy=xy)
+axs.set_xlabel('Recall')
+axs.set_ylabel('FPR')
+axs.title.set_text('FPR at recall')
+fig.savefig(f'{args.save_path}/fpr_at_recall_curves/{best_probes_file_name}_fpr_at_recall.png')
