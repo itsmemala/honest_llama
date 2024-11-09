2a2a288cf95f012a1686e7c09dbf1e1f37c4a325
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-11-09 19:30:38 +0000

    Update plot_manual.py

--- plot_manual.py
+++ plot_manual.py
@@ -8,7 +8,7 @@ paths = ['NLSC42_hl_llama_7B_trivia_qa_greedy_responses_train5000_5000_1_layerFa
 'T42_hl_llama_7B_trivia_qa_sampledplus_responses_train5000_55000_1_layerFalse_answer_last_transformer_supconv2_pos_wp_kmeans_hallu_pos_2.0_mahalanobis_centers1pca0.9_bs352_epochs500_0.0005_Falseba_bestusinglast_fpr_at_recall'
 ]
 labels = ['linear_last_layer','tfmr_all_layers','tfmr_supcon','tfmr_supcon+*','tfmr_supcon+*_dist']
-for path,label in zip(xpaths,labels):
+for path,label in zip(paths,labels):
     recall_vals = np.load(f'{path}_xaxis.npy')
     fpr_at_recall_vals = np.load(f'{path}_yaxis.npy')
     axs.plot(recall_vals,fpr_at_recall_vals,label)
