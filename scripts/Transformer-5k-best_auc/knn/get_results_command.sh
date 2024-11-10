3d7c47f76032d43dab2b5d93ebd622fdf6f833d9
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-11-10 19:19:19 +0000

    upd

--- scripts/Transformer-5k-best_auc/knn/get_results_command.sh
+++ scripts/Transformer-5k-best_auc/knn/get_results_command.sh
@@ -1,5 +1,5 @@
 ############## Transformer 1 layer - n ##################################
-# python analyse_crossdataset_bce_transfmr.py hl_llama_7B trivia_qa --using_act layer --token answer_last --probes_file_name T42_hl_llama_7B_trivia_qa_greedy_responses_train5000_5000_1_layerFalse_answer_last_transformer_knn_hallu_pos_5_bs128_epochs50_ --probes_file_name_concat ba --lr_list 0.00005,0.0005,0.005 --best_threshold True --save_path /home/local/data/ms/honest_llama_data --seed_list 42,101,2650
+# python analyse_crossdataset_bce_transfmr.py hl_llama_7B trivia_qa --using_act layer --token answer_last --probes_file_name T42_hl_llama_7B_trivia_qa_greedy_responses_train5000_5000_1_layerFalse_answer_last_transformer_kmeans_hallu_pos_mahalanobis_centers1pca0.9_bs128_epochs50_ --probes_file_name_concat ba_bestusinglast --lr_list 0.00005,0.0005,0.005 --best_threshold True --save_path /home/local/data/ms/honest_llama_data --seed_list 42,101,2650
 
 # python analyse_crossdataset_bce_transfmr.py hl_llama_7B nq_open --using_act layer --token answer_last --probes_file_name T42_hl_llama_7B_nq_open_greedy_responses_train5000_5000_1_layerFalse_answer_last_transformer_knn_hallu_pos_5_bs128_epochs50_ --probes_file_name_concat ba --lr_list 0.00005,0.0005,0.005 --best_threshold True --save_path /home/local/data/ms/honest_llama_data --seed_list 42,101,2650
 
