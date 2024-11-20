25384d566d909ca7a79343956761d6695fe79472
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-11-20 18:29:35 +0000

    Update analyse_crossdataset_bce.py

--- analyse_crossdataset_bce.py
+++ analyse_crossdataset_bce.py
@@ -179,11 +179,11 @@ def main():
                     # perf = np.mean((cls1_f1,cls0_f1)) # cls1_f1
                     # if perf>best_val_perf:
                     #     best_val_perf, best_t = perf, t
-                    fp = np.sum((val_pred_model == 1) & (np.squeeze(all_val_true[fold][0]) == 0))
-                    tn = np.sum((val_pred_model == 0) & (np.squeeze(all_val_true[fold][0]) == 0))
+                    fp = np.sum((np.squeeze(val_pred_model) == 1) & (np.squeeze(all_val_true[fold][0]) == 0))
+                    tn = np.sum((np.squeeze(val_pred_model) == 0) & (np.squeeze(all_val_true[fold][0]) == 0))
                     val_fpr = fp / (fp + tn)
                     print(fp,tn,np.sum(np.squeeze(all_val_true[fold][0]) == 0))
-                    print(val_pred_model)
+                    print(np.squeeze(val_pred_model))
                     sys.exit()
                     if recall >= 0.95:
                         if val_fpr<best_val_fpr:
