4e75d4c9a669331f32f4ea20ba3ee0f934cbec13
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-11-20 18:26:35 +0000

    Update analyse_crossdataset_bce.py

--- analyse_crossdataset_bce.py
+++ analyse_crossdataset_bce.py
@@ -182,8 +182,8 @@ def main():
                     fp = np.sum((val_pred_model == 1) & (np.squeeze(all_val_true[fold][0]) == 0))
                     tn = np.sum((val_pred_model == 0) & (np.squeeze(all_val_true[fold][0]) == 0))
                     val_fpr = fp / (fp + tn)
-                    print(fp,tn,np.sum(all_val_true[fold][0] == 0))
-                    print(all_val_true[fold][0])
+                    print(fp,tn,np.sum(np.squeeze(all_val_true[fold][0]) == 0))
+                    print(np.squeeze(all_val_true[fold][0]))
                     sys.exit()
                     if recall >= 0.95:
                         if val_fpr<best_val_fpr:
