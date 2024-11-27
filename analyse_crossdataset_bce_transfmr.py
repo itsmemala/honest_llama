8b1fa8a730c7bdc651a1a485b92cc7dc719269c1
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-11-27 17:20:40 +0000

    Update analyse_crossdataset_bce_transfmr.py

--- analyse_crossdataset_bce_transfmr.py
+++ analyse_crossdataset_bce_transfmr.py
@@ -357,7 +357,7 @@ def main():
         # seed_results_list.append(np.mean(best_r))
         # seed_results_list.append(np.mean(test_fpr_best_r))
         if args.fpr_at_recall==-1:
-            print(model)
+            print('model:',model)
             recall_vals, fpr_at_recall_vals, aucfpr = my_aufpr(test_preds[model],labels)
             fig, axs = plt.subplots(1,1)
             axs.plot(recall_vals,fpr_at_recall_vals)
