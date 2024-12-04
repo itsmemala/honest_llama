2332440a1b7e3ed2ec14830f2091bc48a42b39e7
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-12-04 11:08:12 +0000

    Update losses.py

--- losses.py
+++ losses.py
@@ -99,6 +99,7 @@ class SupConLoss(nn.Module):
         )
         mask = mask * logits_mask # this sets the diagonal elements to 0
         print(mask)
+        sys.exit()
         
         # compute log_prob
         exp_logits = torch.exp(logits) * logits_mask
