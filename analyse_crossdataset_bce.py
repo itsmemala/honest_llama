ca5c9436fba03ef37ba073b0d68d74c4b1fc8f08
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-12-04 22:24:54 +0000

    Update analyse_crossdataset_bce.py

--- analyse_crossdataset_bce.py
+++ analyse_crossdataset_bce.py
@@ -92,8 +92,8 @@ def main():
         print('Num of samples positively affected:',len(samples_pos_affected))
     
     # args.using_act = 'layer' if 'layer' in args.probes_file_name else 'mlp'
-    num_layers = 33 if '7B' in args.model_name and args.using_act=='layer' else 32 if '7B' in args.model_name else 40 if '13B' in args.model_name else 60 if '33B' in args.model_name else 0
-    num_models = 33 if args.using_act=='layer' else 32 if args.using_act=='mlp' else 32*32
+    num_layers = 1 #33 if '7B' in args.model_name and args.using_act=='layer' else 32 if '7B' in args.model_name else 40 if '13B' in args.model_name else 60 if '33B' in args.model_name else 0
+    num_models = 1 #33 if args.using_act=='layer' else 32 if args.using_act=='mlp' else 32*32
     if ('knn' in args.probes_file_name) or ('kmeans' in args.probes_file_name): 
         print('\n\nSETTING NUM_LAYERS=1\n\n')
         num_layers, num_models = 1, 1 # We only ran these for the last layer
