8fe142acd03da77bd31e574296cc0de62f1e23fb
Malavika Suresh <itsmemala@users.noreply.github.com>
2024-12-07 15:07:37 +0000

    Update analyse_crossdataset_bce.py

--- analyse_crossdataset_bce.py
+++ analyse_crossdataset_bce.py
@@ -53,6 +53,7 @@ def main():
     parser.add_argument('--seed_list',default=None,type=list_of_ints,required=False,help='(default=%(default)s)')
     parser.add_argument("--best_hyp_using_aufpr", type=bool, default=False, help='local directory with dataset')
     parser.add_argument("--best_threshold", type=bool, default=False, help='local directory with dataset')
+    parser.add_argument("--best_thr_using_recall", type=bool, default=False, help='local directory with dataset')
     parser.add_argument('--fpr_at_recall',type=float, default=0.95)
     parser.add_argument('--aufpr_from',type=float, default=0.0)
     parser.add_argument('--aufpr_till',type=float, default=1.0)
@@ -237,8 +238,10 @@ def main():
             else:
                 all_val_pred, all_val_true = np.load(f'{args.save_path}/probes/{best_probes_file_name}_val_pred.npy', allow_pickle=True).item(), np.load(f'{args.save_path}/probes/{best_probes_file_name}_val_true.npy', allow_pickle=True).item()
             if args.best_threshold:
-                best_val_perf, best_t = 0, 0.5
-                # best_val_fpr, best_t = 1, 0
+                if args.best_thr_using_recall:
+                    best_val_fpr, best_t = 1, 0
+                else:
+                    best_val_perf, best_t = 0, 0.5
                 val_dist_min, val_dist_max = all_val_pred[fold][model].min(), all_val_pred[fold][model].max()
                 if args.min_max_scale_dist: all_val_pred[fold][model] = (all_val_pred[fold][model] - val_dist_min) / (val_dist_max - val_dist_min) # min-max-scale distances
                 # thresholds = np.histogram_bin_edges(all_val_pred[fold][model], bins='sqrt') if ('knn' in args.probes_file_name) or ('kmeans' in args.probes_file_name) else [0,0.05,0.10,0.15,0.20,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95,1.0]
@@ -257,15 +260,17 @@ def main():
                     cls1_f1 = f1_score(all_val_true[fold][0],val_pred_model)
                     cls0_f1 = f1_score(all_val_true[fold][0],val_pred_model,pos_label=0)
                     recall = recall_score(all_val_true[fold][0],val_pred_model)
-                    perf = np.mean((cls1_f1,cls0_f1)) # cls1_f1
-                    if perf>best_val_perf:
-                        best_val_perf, best_t = perf, t
-                    # fp = np.sum((np.squeeze(val_pred_model) == 1) & (np.squeeze(all_val_true[fold][0]) == 0))
-                    # tn = np.sum((np.squeeze(val_pred_model) == 0) & (np.squeeze(all_val_true[fold][0]) == 0))
-                    # val_fpr = fp / (fp + tn)
-                    # if recall >= 0.9:
-                    #     if val_fpr<best_val_fpr:
-                    #         best_val_fpr, best_t = val_fpr, t
+                    if args.best_thr_using_recall:
+                        perf = np.mean((cls1_f1,cls0_f1)) # cls1_f1
+                        if perf>best_val_perf:
+                            best_val_perf, best_t = perf, t
+                    else:
+                        fp = np.sum((np.squeeze(val_pred_model) == 1) & (np.squeeze(all_val_true[fold][0]) == 0))
+                        tn = np.sum((np.squeeze(val_pred_model) == 0) & (np.squeeze(all_val_true[fold][0]) == 0))
+                        val_fpr = fp / (fp + tn)
+                        if recall >= 0.9:
+                            if val_fpr<best_val_fpr:
+                                best_val_fpr, best_t = val_fpr, t
             else:
                 best_t = 0.5
             return best_probes_file_name, all_val_pred, all_val_true, best_t, val_dist_min, val_dist_max
