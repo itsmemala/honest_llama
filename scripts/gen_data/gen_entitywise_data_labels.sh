f73ea113b9cfc580f7701e0da49010f163a519e9
Malavika Suresh <itsmemala@users.noreply.github.com>
2025-05-24 14:57:30 +0100

    sync

--- scripts/gen_data/gen_entitywise_data_labels.sh
+++ scripts/gen_data/gen_entitywise_data_labels.sh
@@ -13,3 +13,20 @@
 # python get_prompt_responses_factual_onlylabels.py hl_llama_7B movie_cast --len_dataset 0 --start_at 0 --use_split test --device 0 --save_path /home/local/data/ms/honest_llama_data
 # accelerate launch --num_processes 2 --multi_gpu get_prompt_responses_factual_parallel_fast.py hl_llama_7B movie_cast --len_dataset 1000 --start_at 0 --use_split train --save_path /home/local/data/ms/honest_llama_data --do_sample True --num_ret_seq 10
 # accelerate launch --num_processes 2 --multi_gpu get_prompt_responses_factual_parallel_fast.py hl_llama_7B movie_cast --len_dataset 0 --start_at 0 --use_split test --device 0 --save_path /home/local/data/ms/honest_llama_data --do_sample True --num_ret_seq 10
+
+
+##### Gemma 2B ########
+accelerate launch --num_processes 2 --multi_gpu  get_prompt_responses_factual_parallel.py gemma_2B city_country --len_dataset 0 --start_at 0 --use_split train --device 0 --save_path /home/local/data/ms/honest_llama_data;
+printf "City train completed";
+accelerate launch --num_processes 2 --multi_gpu  get_prompt_responses_factual_parallel.py gemma_2B city_country --len_dataset 0 --start_at 0 --use_split test --device 0 --save_path /home/local/data/ms/honest_llama_data;
+printf "City test completed";
+
+accelerate launch --num_processes 2 --multi_gpu  get_prompt_responses_factual_parallel.py gemma_2B player_date_birth --len_dataset 0 --start_at 0 --use_split train --device 0 --save_path /home/local/data/ms/honest_llama_data;
+printf "Player train completed";
+accelerate launch --num_processes 2 --multi_gpu  get_prompt_responses_factual_parallel.py gemma_2B player_date_birth --len_dataset 0 --start_at 0 --use_split test --device 0 --save_path /home/local/data/ms/honest_llama_data;
+printf "Player test completed";
+
+accelerate launch --num_processes 2 --multi_gpu  get_prompt_responses_factual_parallel.py gemma_2B movie_cast --len_dataset 0 --start_at 0 --use_split train --device 0 --save_path /home/local/data/ms/honest_llama_data;
+printf "Movie train completed";
+accelerate launch --num_processes 2 --multi_gpu  get_prompt_responses_factual_parallel.py gemma_2B movie_cast --len_dataset 0 --start_at 0 --use_split test --device 0 --save_path /home/local/data/ms/honest_llama_data;
+printf "Movie test completed";
