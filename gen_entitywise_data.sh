0523241b5ecd19c9e05a3f79e3e4e2d9a024f10a
Malavika Suresh <itsmemala@users.noreply.github.com>
2025-06-05 13:00:45 +0100

    Create gen_entitywise_data.sh

---
+++ gen_entitywise_data.sh
@@ -0,0 +1,31 @@
+# python get_activations.py hl_llama_7B city_country --token answer_last --file_name city_country_greedy_responses_train6323  --device 0 --save_path /home/local/data/ms/honest_llama_data
+# python get_activations.py hl_llama_7B city_country --token answer_last --file_name city_country_greedy_responses_test1581  --device 0 --save_path /home/local/data/ms/honest_llama_data
+# python get_activations.py hl_llama_7B city_country --token answer_last --file_name city_country_sampledplus_responses_train1000  --device 0 --save_path /home/local/data/ms/honest_llama_data --num_samples 11
+# python get_activations.py hl_llama_7B city_country --token answer_last --file_name city_country_sampled_responses_test1581  --device 0 --save_path /home/local/data/ms/honest_llama_data --num_samples 10
+
+# python get_activations.py hl_llama_7B player_date_birth --token answer_last --file_name player_date_birth_greedy_responses_test1498  --device 0 --save_path /home/local/data/ms/honest_llama_data
+# python get_activations.py hl_llama_7B player_date_birth --token answer_last --file_name player_date_birth_greedy_responses_train5989  --device 0 --save_path /home/local/data/ms/honest_llama_data
+# python get_activations.py hl_llama_7B player_date_birth --token answer_last --file_name player_date_birth_sampledplus_responses_train1000  --device 0 --save_path /home/local/data/ms/honest_llama_data --num_samples 11
+# python get_activations.py hl_llama_7B player_date_birth --token answer_last --file_name player_date_birth_sampled_responses_test1498  --device 0 --save_path /home/local/data/ms/honest_llama_data --num_samples 10
+
+# python get_activations_hl.py hl_llama_7B movie_cast --token answer_last --file_name movie_cast_greedy_responses_train8716 --device 0 --save_path /home/local/data/ms/honest_llama_data
+# python get_activations_hl.py hl_llama_7B movie_cast --token answer_last --file_name movie_cast_greedy_responses_test2179  --device 0 --save_path /home/local/data/ms/honest_llama_data
+# python get_activations.py hl_llama_7B movie_cast --token answer_last --file_name movie_cast_sampled_responses_test2179  --device 0 --save_path /home/local/data/ms/honest_llama_data --num_samples 10
+
+
+
+### Gemma 2B ####
+# python get_activations.py gemma_2B city_country --token answer_last --file_name city_country_greedy_responses_train6323  --device 0 --save_path /home/local/data/ms/honest_llama_data
+# python get_activations.py gemma_2B city_country --token answer_last --file_name city_country_greedy_responses_test1581  --device 0 --save_path /home/local/data/ms/honest_llama_data
+# python get_activations.py gemma_2B player_date_birth --token answer_last --file_name player_date_birth_greedy_responses_test1498  --device 0 --save_path /home/local/data/ms/honest_llama_data
+# python get_activations.py gemma_2B player_date_birth --token answer_last --file_name player_date_birth_greedy_responses_train5989  --device 0 --save_path /home/local/data/ms/honest_llama_data
+# python get_activations.py gemma_2B movie_cast --token answer_last --file_name movie_cast_greedy_responses_train8716 --device 0 --save_path /home/local/data/ms/honest_llama_data
+# python get_activations.py gemma_2B movie_cast --token answer_last --file_name movie_cast_greedy_responses_test2179  --device 0 --save_path /home/local/data/ms/honest_llama_data
+
+### Alpaca 7B ####
+python get_activations.py alpaca_7B city_country --token answer_last --file_name city_country_greedy_responses_train6323  --device 0 --save_path /home/local/data/ms/honest_llama_data
+python get_activations.py alpaca_7B city_country --token answer_last --file_name city_country_greedy_responses_test1581  --device 0 --save_path /home/local/data/ms/honest_llama_data
+python get_activations.py alpaca_7B player_date_birth --token answer_last --file_name player_date_birth_greedy_responses_test1498  --device 0 --save_path /home/local/data/ms/honest_llama_data
+python get_activations.py alpaca_7B player_date_birth --token answer_last --file_name player_date_birth_greedy_responses_train5989  --device 0 --save_path /home/local/data/ms/honest_llama_data
+python get_activations.py alpaca_7B movie_cast --token answer_last --file_name movie_cast_greedy_responses_train8716 --device 0 --save_path /home/local/data/ms/honest_llama_data
+python get_activations.py alpaca_7B movie_cast --token answer_last --file_name movie_cast_greedy_responses_test2179  --device 0 --save_path /home/local/data/ms/honest_llama_data
